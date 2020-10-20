1fb4480c9096ab74afcfd0ed337001dc6607b4ba
smarcet <smarcet@gmail.com>
2020-10-19 21:35:30 -0300

    Updated CORS middleware
    Change-Id: Icedcd22e6e73d304583e682647931f85b761a7f8
    Signed-off-by: smarcet <smarcet@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\CORSMiddleware::class,
+        \Spatie\Cors\Cors::class,
         \App\Http\Middleware\SecurityHTTPHeadersWriterMiddleware::class,
         \App\Http\Middleware\ParseMultipartFormDataInputForNonPostRequests::class,
     ];

a21d1a387988845d6e30e8757921443580d75359
smarcet@gmail.com <smarcet@gmail.com>
2021-10-26 17:53:36 -0300

    Added cache value for file urls (download estrategy)
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>
    Change-Id: Ie4cc34b19b335ea789341eda5dc5169c5c69c47b

--- config/cache_api_response.php
+++ config/cache_api_response.php
@@ -17,4 +17,5 @@ return [
     'get_event_feedback_response_lifetime'  => env('CACHE_API_RESPONSE_GET_EVENT_FEEDBACK_LIFETIME', 300),
     'get_published_event_response_lifetime' => env('CACHE_API_RESPONSE_GET_PUBLISHED_EVENT_LIFETIME', 300),
     'get_summits_response_lifetime'         => env('CACHE_API_RESPONSE_GET_SUMMITS_LIFETIME', 600),
+    'file_url_lifetime'                     => env('CACHE_API_RESPONSE_FILE_URL_LIFETIME', 3600),
 ];

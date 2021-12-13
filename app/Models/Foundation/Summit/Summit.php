99438c6a6173619f436eabb8bedf87305b3024f5
smarcet@gmail.com <smarcet@gmail.com>
2021-12-13 15:49:04 -0600

    Removed log info
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>
    Change-Id: I5f02a459c5d0ff6032c44f796e0d9b207814ceeb

--- app/Models/Foundation/Summit/Summit.php
+++ app/Models/Foundation/Summit/Summit.php
@@ -5284,12 +5284,10 @@ SQL;
         $days = $this->getSummitDays();
         $list = [];
         foreach ($days as $day) {
-            Log::debug(sprintf("Summit::getSummitDaysWithEvents day %s", $day->format('Y-m-d H:i:s')));
             $begin = clone($day);
             $begin = $begin->setTime(0, 0, 0)->setTimezone(new \DateTimeZone('UTC'));
             $end = clone($day);
             $end = $end->setTime(23, 59, 59)->setTimezone(new \DateTimeZone('UTC'));
-            Log::debug(sprintf("Summit::getSummitDaysWithEvents UTC begin date %s UTC end date %s", $begin->format('Y-m-d H:i:s'), $end->format('Y-m-d H:i:s')));
             $count = 0;
             try {
                 $sql = <<<SQL
@@ -5308,7 +5306,6 @@ SQL;
                 ]);
                 $res = $stmt->fetchAll(\PDO::FETCH_COLUMN);
                 $count = count($res) > 0 ? $res[0] : 0;
-                Log::debug(sprintf("Summit::getSummitDaysWithEvents summit %s begin %s end %s count %s", $this->id, $begin->format("Y-m-d H:i:s"), $end->format('Y-m-d H:i:s'), $count));
             } catch (\Exception $ex) {
                 Log::debug($ex);
                 $count = 0;

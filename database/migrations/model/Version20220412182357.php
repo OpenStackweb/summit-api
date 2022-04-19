a9535a345751e7d2642395e2237afadd31c0e967
smarcet@gmail.com <smarcet@gmail.com>
2022-04-18 20:57:24 -0500

    Fix on migration
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>
    Change-Id: I32a0dde55e96c48295aaa18e548d65d397aa4714

--- database/migrations/model/Version20220412182357.php
+++ database/migrations/model/Version20220412182357.php
@@ -13,12 +13,19 @@ class Version20220412182357 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $sql = <<<SQL
-INSERT INTO SelectionPlan_SummitEventTypes (SelectionPlanID, SummitEventTypeID) 
-SELECT SelectionPlan.ID, SummitEventType.ID FROM SelectionPlan, SummitEventType 
-WHERE SummitEventType.ClassName = 'PresentationType' AND SummitEventType.SummitID = SelectionPlan.ID AND NOT EXISTS (
-    SELECT 1 FROM SelectionPlan_SummitEventTypes 
-    WHERE SelectionPlan_SummitEventTypes.SelectionPlanID = SelectionPlan.ID 
-            AND SelectionPlan_SummitEventTypes.SummitEventTypeID = SummitEventType.ID
+INSERT INTO SelectionPlan_SummitEventTypes (SelectionPlanID, SummitEventTypeID)
+SELECT SelectionPlan.ID, SummitEventType.ID
+FROM SelectionPlan, SummitEventType
+INNER JOIN PresentationType ON PresentationType.ID = SummitEventType.ID
+WHERE
+      SummitEventType.ClassName = 'PresentationType'
+      AND PresentationType.ShouldBeAvailableOnCFP = 1
+      AND SummitEventType.SummitID = SelectionPlan.SummitID
+      AND NOT EXISTS (
+            SELECT 1 FROM SelectionPlan_SummitEventTypes
+            WHERE
+                  SelectionPlan_SummitEventTypes.SelectionPlanID = SelectionPlan.ID
+                  AND SelectionPlan_SummitEventTypes.SummitEventTypeID = SummitEventType.ID
     );
 SQL;
         $this->addSql($sql);

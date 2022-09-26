ac4aea810fe17b57abf0180edebddf5ea649cc49
smarcet@gmail.com <smarcet@gmail.com>
2022-09-26 12:41:48 -0500

    Fixed order update
    fixed update owner procedure
    
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>
    Change-Id: I2a76cbed7c5b4d199a3886f6843d8b6f0bce2cb3

---
+++ database/migrations/model/Version20220926172743.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Migrations\Model;
+
+use Database\Utils\DBHelpers;
+use Doctrine\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema as Schema;
+
+class Version20220926172743 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        if(!DBHelpers::existsFK(env('SS_DATABASE'), "SummitOrder", "FK_SummitOrder_Owner")) {
+            $sql = <<<SQL
+ALTER TABLE `SummitOrder` ADD CONSTRAINT 
+    `FK_SummitOrder_Owner` FOREIGN KEY (`OwnerID`) 
+        REFERENCES `Member`(`ID`) ON DELETE SET NULL ON UPDATE RESTRICT;
+SQL;
+
+            $this->addSql($sql);
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+
+    }
+}

c17aba63c106e73eaf614247359ba72891debe01
smarcet@gmail.com <smarcet@gmail.com>
2022-11-09 15:48:13 -0600

    Updated Presentation.social_description to 300 chars
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>
    Change-Id: I4ca9da5a9129e91ac3220b520bd377f851902768

---
+++ database/migrations/model/Version20221109214446.php
@@ -0,0 +1,41 @@
+<?php namespace Database\Migrations\Model;
+/**
+ * Copyright 2022 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+use Doctrine\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema as Schema;
+
+/**
+ * Class Version20221109214446
+ * @package Database\Migrations\Model
+ */
+final class Version20221109214446 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $sql = <<<SQL
+        ALTER TABLE `SummitEvent` CHANGE `SocialSummary` `SocialSummary` VARCHAR(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL;
+SQL;
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+
+    }
+}

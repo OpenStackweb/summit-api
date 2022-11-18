a959c3317b582738bf272698e784c4ee1e8e87bf
romanetar <roman_ag@hotmail.com>
2022-11-18 18:40:46 +0100

    Validate if action type is attached to any selection plan before remove it globally
    Signed-off-by: romanetar <roman_ag@hotmail.com>

--- app/Models/Foundation/Summit/Summit.php
+++ app/Models/Foundation/Summit/Summit.php
@@ -5709,7 +5709,6 @@ SQL;
     {
         if (!$this->presentation_action_types->contains($presentation_action_type)) return $this;
         $this->presentation_action_types->removeElement($presentation_action_type);
-        $presentation_action_type->setSummit(null);
         return $this;
     }
 

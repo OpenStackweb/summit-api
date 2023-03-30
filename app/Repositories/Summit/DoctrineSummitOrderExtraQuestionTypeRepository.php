dc34f84924ef9d2514ca27b5837df1ac79d44b12
romanetar <roman_ag@hotmail.com>
2023-03-30 14:46:27 +0200

    Model - ORM Mappings & Serializers
    Signed-off-by: romanetar <roman_ag@hotmail.com>

--- app/Repositories/Summit/DoctrineSummitOrderExtraQuestionTypeRepository.php
+++ app/Repositories/Summit/DoctrineSummitOrderExtraQuestionTypeRepository.php
@@ -14,7 +14,14 @@
 use App\Models\Foundation\Summit\Repositories\ISummitOrderExtraQuestionTypeRepository;
 use App\Repositories\Main\DoctrineExtraQuestionTypeRepository;
 use models\summit\SummitOrderExtraQuestionType;
+use utils\DoctrineCaseFilterMapping;
 use utils\DoctrineLeftJoinFilterMapping;
+use utils\DoctrineSwitchFilterMapping;
+use utils\Filter;
+use utils\Order;
+use utils\PagingInfo;
+use utils\PagingResponse;
+
 /**
  * Class DoctrineSummitOrderExtraQuestionTypeRepository
  * @package App\Repositories\Summit
@@ -32,7 +39,31 @@ final class DoctrineSummitOrderExtraQuestionTypeRepository
         return array_merge(parent::getFilterMappings() , [
             'printable' => 'e.printable:json_boolean',
             'usage'     => 'e.usage:json_string',
-            'summit_id' => new DoctrineLeftJoinFilterMapping("e.summit", "s" ,"s.id :operator :value")
+            'summit_id' => new DoctrineLeftJoinFilterMapping("e.summit", "s" ,"s.id :operator :value"),
+            'has_badge_feature_types' =>
+                new DoctrineSwitchFilterMapping([
+                        'true' => new DoctrineCaseFilterMapping(
+                            'true',
+                            'SIZE(e.allowed_badge_features_types) > 0'
+                        ),
+                        'false' => new DoctrineCaseFilterMapping(
+                            'false',
+                            'SIZE(e.allowed_badge_features_types) = 0'
+                        ),
+                    ]
+                ),
+            'has_ticket_types' =>
+                new DoctrineSwitchFilterMapping([
+                        'true' => new DoctrineCaseFilterMapping(
+                            'true',
+                            'SIZE(e.allowed_ticket_types) > 0'
+                        ),
+                        'false' => new DoctrineCaseFilterMapping(
+                            'false',
+                            'SIZE(e.allowed_ticket_types) = 0'
+                        ),
+                    ]
+                ),
         ]);
     }
 
@@ -51,5 +82,27 @@ final class DoctrineSummitOrderExtraQuestionTypeRepository
     protected function getBaseEntity()
     {
         return SummitOrderExtraQuestionType::class;
+    }
+
+    /**
+     * @param PagingInfo $paging_info
+     * @param Filter|null $filter
+     * @param Order|null $order
+     * @return PagingResponse
+     */
+    public function getAllByPage(PagingInfo $paging_info, Filter $filter = null, Order $order = null)
+    {
+        return $this->getParametrizedAllByPage(function () {
+            return $this->getEntityManager()->createQueryBuilder()
+                ->select("e")
+                ->from($this->getBaseEntity(), "e");
+        },
+            $paging_info,
+            $filter,
+            $order,
+            function ($query) {
+                //default order
+                return $query;
+            });
     }
 }

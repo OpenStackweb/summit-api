21dc036b078d218bbd31131023237ef432319f6e
smarcet@gmail.com <smarcet@gmail.com>
2023-10-24 18:18:45 -0300

    Fixes and tweaks
    Change-Id: Icb9ccae958e9fd2d557aefe539d87f06606e847d

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1372,6 +1372,7 @@ Route::group(array('prefix' => 'summits'), function () {
 
         // tickets
         Route::group(['prefix' => 'tickets'], function () {
+
             Route::get('', ['middleware' => 'auth.user', 'uses' => 'OAuth2SummitTicketApiController@getAllBySummit']);
             Route::get('external', ['middleware' => 'auth.user', 'uses' => 'OAuth2SummitTicketApiController@getAllBySummitExternal']);
 
@@ -1383,12 +1384,15 @@ Route::group(array('prefix' => 'summits'), function () {
 
             Route::post('ingest', ['middleware' => 'auth.user', 'uses' => 'OAuth2SummitTicketApiController@ingestExternalTicketData']);
 
-            Route::group(['prefix' => '{ticket_id}'], function () {
+            Route::group(['prefix' => '{ticket_id}', 'where' => ['ticket_id' => '[0-9]+']], function () {
+
                 Route::get('', ['middleware' => 'auth.user', 'uses' => 'OAuth2SummitTicketApiController@get']);
                 // badge endpoints
                 Route::group(['prefix' => 'badge'], function () {
+
                     Route::post('', ['middleware' => 'auth.user', 'uses' => 'OAuth2SummitTicketApiController@createAttendeeBadge']);
                     Route::get('', ['middleware' => 'auth.user', 'uses' => 'OAuth2SummitTicketApiController@getAttendeeBadge']);
+
                     Route::group(['prefix' => 'current'], function () {
                         Route::delete('', ['middleware' => 'auth.user', 'uses' => 'OAuth2SummitTicketApiController@deleteAttendeeBadge']);
 
---
+++ tests/OAuth2SummitAttendeeBadgePrintApiControllerTest.php
@@ -0,0 +1,74 @@
+<?php namespace Tests;
+/*
+ * Copyright 2023 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+
+use App\Models\Foundation\Main\IGroup;
+use LaravelDoctrine\ORM\Facades\EntityManager;
+use models\summit\Summit;
+use models\summit\SummitAttendeeBadge;
+
+final class OAuth2SummitAttendeeBadgePrintApiControllerTest extends ProtectedApiTest
+{
+    use InsertSummitTestData;
+
+    use InsertMemberTestData;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        self::insertMemberTestData(IGroup::FoundationMembers);
+        self::$defaultMember = self::$member;
+        self::$defaultMember2 = self::$member2;
+        self::insertSummitTestData();
+    }
+
+    protected function tearDown(): void
+    {
+        self::clearSummitTestData();
+        parent::tearDown();
+    }
+
+    public function testDeletePrintsFromBadge(){
+        $attendee = self::$summit->getAttendees()[0];
+        $ticket = $attendee->getFirstTicket();
+        $this->assertTrue(!is_null($ticket));
+        $badge = $ticket->getBadge();
+        $this->assertTrue(!is_null($badge));
+        // create test print
+        $badge->printIt(self::$member, self::$default_badge_view_type);
+        self::$em->flush();
+
+        $params = [
+            'id'          => self::$summit->getId(),
+            'ticket_id'   => $ticket->getId(),
+        ];
+
+        $this->action(
+            "DELETE",
+            "OAuth2SummitAttendeeBadgePrintApiController@deleteBadgePrints",
+            $params,
+            [],
+            [],
+            [],
+            $this->getAuthHeaders(),
+        );
+
+        $this->assertResponseStatus(204);
+
+        $badge_repository = EntityManager::getRepository(SummitAttendeeBadge::class);
+        $badge_from_db = $badge_repository->find($badge->getId());
+        $this->assertTrue(!is_null($badge_from_db));
+        $this->assertFalse($badge_from_db->isPrinted());
+    }
+}

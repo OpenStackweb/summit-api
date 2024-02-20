30a4bf8aefd921b0e1909036136a92e28e2638a8
smarcet@gmail.com <smarcet@gmail.com>
2024-02-20 16:24:32 -0300

    fix: reduce to 5 minutes the attendee email threashold
    Change-Id: Icaf0c82e162fc5b9e6da642e9c9239605851c979

--- app/Models/Foundation/Summit/Registration/Attendees/SummitAttendee.php
+++ app/Models/Foundation/Summit/Registration/Attendees/SummitAttendee.php
@@ -500,8 +500,8 @@ class SummitAttendee extends SilverstripeBaseModel
             // regenerate the QR code if the ticket is complete
             $ticket->generateQRCode();
             Log::debug(sprintf("SummitAttendee::sendInvitationEmail attendee %s is complete", $email));
-            // adds a threshold of 20 minutes to avoid duplicates emails
-            if (Cache::add(sprintf("%s_emit_ticket", $key), true, now()->addMinutes(20))) {
+            // adds a threshold of 5 minutes to avoid duplicates emails
+            if (Cache::add(sprintf("%s_emit_ticket", $key), true, now()->addMinutes(5))) {
                 Log::debug(sprintf("SummitAttendee::sendInvitationEmail attendee %s ticket %s sending SummitAttendeeTicketEmail", $email, $ticket->getId()));
                 SummitAttendeeTicketEmail::dispatch($ticket, $payload, $test_email_recipient);
                 $ticket->getOwner()->markInvitationEmailSentDate();
@@ -517,8 +517,8 @@ class SummitAttendee extends SilverstripeBaseModel
         // they bought a ticket for themselves.
         if ($order->getOwnerEmail() !== $ticket->getOwnerEmail() || $overrideTicketOwnerIsSameAsOrderOwnerRule) {
             // no delay
-            // adds a threshold of 20 minutes to avoid duplicates emails
-            if (Cache::add(sprintf("%s_edit_ticket", $key), true, now()->addMinutes(20))) {
+            // adds a threshold of 5 minutes to avoid duplicates emails
+            if (Cache::add(sprintf("%s_edit_ticket", $key), true, now()->addMinutes(5))) {
                 Log::debug(sprintf("SummitAttendee::sendInvitationEmail attendee %s ticket %s sending InviteAttendeeTicketEditionMail", $email, $ticket->getId()));
                 InviteAttendeeTicketEditionMail::dispatch($ticket, $payload, $test_email_recipient);
                 $ticket->getOwner()->markInvitationEmailSentDate();

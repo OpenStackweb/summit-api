83c59d47353d3df21e192120684239e29bca47de
romanetar <roman_ag@hotmail.com>
2024-06-06 19:31:00 +0200

    chore: docker dev environment setup
    Signed-off-by: romanetar <roman_ag@hotmail.com>

--- docker-compose/mysql/01_databases.sql
+++
@@ -1,8 +0,0 @@
--- create databases
-CREATE DATABASE IF NOT EXISTS `api_test`;
-CREATE DATABASE IF NOT EXISTS `model_test`;
-
--- create root user and grant rights
-CREATE USER 'root'@'localhost' IDENTIFIED BY '1qaz2wsx!';
-GRANT ALL ON *.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;

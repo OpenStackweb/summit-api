2e04808d872a49c15a1682550c734ca69cfa173a
romanetar <roman_ag@hotmail.com>
2024-06-06 19:31:00 +0200

    chore: docker dev environment setup
    Signed-off-by: romanetar <roman_ag@hotmail.com>

---
+++ database/migrations/config/initial_migrations.sql
@@ -0,0 +1,4 @@
+INSERT INTO DoctrineMigration (version, executed_at) VALUES ('20190422160409', '2019-04-22 16:04:09');
+INSERT INTO DoctrineMigration (version, executed_at) VALUES ('20190828142430', '2019-08-28 14:24:30');
+INSERT INTO DoctrineMigration (version, executed_at) VALUES ('20190828143005', '2019-08-28 14:30:05');
+INSERT INTO DoctrineMigration (version, executed_at) VALUES ('20200123174717', '2020-01-23 17:47:17');
--- start_local_server.sh
+++ start_local_server.sh
@@ -3,8 +3,9 @@ set -e
 export DOCKER_SCAN_SUGGEST=false
 
 docker compose run --rm app composer install --ignore-platform-req=ext-gd --ignore-platform-req=ext-imagick
-docker compose run --rm app php artisan doctrine:migrations:migrate --em=model --no-interaction
-docker compose run --rm app php artisan doctrine:migrations:migrate --em=config --no-interaction
+docker compose run --rm app php artisan db:create_test_db --schema=config
+docker compose run --rm app php artisan db:create_test_db --schema=model
 docker compose run --rm app php artisan db:seed --force
+docker compose run --rm app php artisan db:seed_test_data
 docker compose up -d
 docker compose exec app /bin/bash

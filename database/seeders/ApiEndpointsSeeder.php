55de3a6c8dd2ee069d96f5eefa8b9031f8292973
smarcet@gmail.com <smarcet@gmail.com>
2024-06-20 13:12:33 -0300

    fix: api config seed
    Change-Id: I0a4f5b77aae867f0bfb3c1a8f8fe05c8d251e3d2

--- database/seeders/ApiEndpointsSeeder.php
+++ database/seeders/ApiEndpointsSeeder.php
@@ -32,9 +32,6 @@ class ApiEndpointsSeeder extends Seeder
 
     public function run()
     {
-        DB::table('endpoint_api_scopes')->delete();
-        DB::table('endpoint_api_authz_groups')->delete();
-        DB::table('api_endpoints')->delete();
 
         $this->seedSummitEndpoints();
         $this->seedAuditLogEndpoints();

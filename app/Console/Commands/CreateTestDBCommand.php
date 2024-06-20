3d2f3f610515ce0014b2cc1b737e2f0fdedca4bd
smarcet@gmail.com <smarcet@gmail.com>
2024-06-20 11:35:46 -0300

    chore: add github action to run unit test on push
    Change-Id: I4740f40a603b11289562b97536bed27fcf3e9248

--- app/Console/Commands/CreateTestDBCommand.php
+++ app/Console/Commands/CreateTestDBCommand.php
@@ -21,138 +21,138 @@ use Illuminate\Validation\ValidationException;
  * @package App\Console\Commands
  */
 final class CreateTestDBCommand extends Command {
-  /**
-   * The console command name.
-   *
-   * @var string
-   */
-  protected $name = "create_test_db";
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = "create_test_db";
 
-  /**
-   * The name and signature of the console command.
-   *
-   * @var string
-   */
-  protected $signature = "db:create_test_db {--schema=}";
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = "db:create_test_db {--schema=}";
 
-  /**
-   * The console command description.
-   *
-   * @var string
-   */
-  protected $description = "Create Test DB";
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Create Test DB";
 
-  const SchemaConfig = "config";
+    const SchemaConfig = "config";
 
-  const SchemaModel = "model";
+    const SchemaModel = "model";
 
-  const AllowedSchemas = [self::SchemaConfig, self::SchemaModel];
+    const AllowedSchemas = [self::SchemaConfig, self::SchemaModel];
 
-  /**
-   * Execute the console command.
-   *
-   * @return void
-   */
-  public function handle(): void {
-    $schema_name = $this->option("schema");
-    $this->validateOptions($schema_name);
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void {
+        $schema_name = $this->option("schema");
+        $this->validateOptions($schema_name);
 
-    $this->info(sprintf("Creating Test DB for schema %s", $schema_name));
+        $this->info(sprintf("Creating Test DB for schema %s", $schema_name));
 
-    $db_host = env("SS_DB_HOST");
-    $db_port = env("SS_DB_PORT");
-    $db_user_name = env("SS_DB_USERNAME");
-    $db_password = env("SS_DB_PASSWORD");
-    $db_name = env("SS_DATABASE");
+        $db_host = env("SS_DB_HOST");
+        $db_port = env("SS_DB_PORT");
+        $db_user_name = env("SS_DB_USERNAME");
+        $db_password = env("SS_DB_PASSWORD");
+        $db_name = env("SS_DATABASE");
 
-    if ($schema_name == self::SchemaConfig) {
-      $db_host = env("DB_HOST");
-      $db_port = env("DB_PORT");
-      $db_user_name = env("DB_USERNAME");
-      $db_password = env("DB_PASSWORD");
-      $db_name = env("DB_DATABASE");
-    }
-
-    $pdo = new \PDO(
-      sprintf("mysql:host=%s;port=%s", $db_host, $db_port),
-      $db_user_name,
-      $db_password,
-    );
-    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
-    try {
-      $this->info("dropping schema {$db_name} at host {$db_host}...");
-      $pdo->exec("DROP SCHEMA " . $db_name . ";");
-    } catch (\Exception $e) {
-      $this->error($e->getMessage());
-    }
-
-    try {
-      $this->info("creating schema {$db_name} at host {$db_host}...");
-      $pdo->exec("CREATE SCHEMA " . $db_name . ";");
-      $pdo->exec("USE " . $db_name . ";");
-    } catch (\Exception $e) {
-      $this->error($e->getMessage());
-    }
-
-    $current_dir = dirname(__FILE__);
-
-    try {
-      $this->info("creating initial schema...");
-      $schema = file_get_contents(
-        "{$current_dir}/../../../database/migrations/{$schema_name}/initial_schema.sql",
-        true,
-      );
-      $schema = explode(";", $schema);
-      foreach ($schema as $ddl) {
-        $ddl = trim($ddl);
-        if (empty(trim($ddl))) {
-          continue;
+        if ($schema_name == self::SchemaConfig) {
+            $db_host = env("DB_HOST");
+            $db_port = env("DB_PORT");
+            $db_user_name = env("DB_USERNAME");
+            $db_password = env("DB_PASSWORD");
+            $db_name = env("DB_DATABASE");
         }
-        $pdo->exec($ddl . ";");
-      }
-    } catch (\Exception $e) {
-      $this->error($e->getMessage());
-    }
 
-    try {
-      $this->info("adding already ran migrations...");
-      $migrations = file_get_contents(
-        "{$current_dir}/../../../database/migrations/{$schema_name}/initial_migrations.sql",
-        true,
-      );
+        $pdo = new \PDO(
+            sprintf("mysql:host=%s;port=%s", $db_host, $db_port),
+            $db_user_name,
+            $db_password,
+        );
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
-      $migrations = explode(";", $migrations);
-
-      foreach ($migrations as $idx => $statement) {
-        if (empty(trim($statement))) {
-          continue;
+        try {
+            $this->info("dropping schema {$db_name} at host {$db_host}...");
+            $pdo->exec("DROP SCHEMA " . $db_name . ";");
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
         }
-        $pdo->exec($statement . ";");
-        $this->info("adding migration {$idx} ...");
-      }
 
-      $this->info(sprintf("Test DB for schema %s created successfully!", $schema_name));
-    } catch (\Exception $e) {
-      $this->error($e->getMessage());
+        try {
+            $this->info("creating schema {$db_name} at host {$db_host}...");
+            $pdo->exec("CREATE SCHEMA " . $db_name . ";");
+            $pdo->exec("USE " . $db_name . ";");
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+
+        $current_dir = dirname(__FILE__);
+
+        try {
+            $this->info("creating initial schema...");
+            $schema = file_get_contents(
+                "{$current_dir}/../../../database/migrations/{$schema_name}/initial_schema.sql",
+                true,
+            );
+            $schema = explode(";", $schema);
+            foreach ($schema as $ddl) {
+                $ddl = trim($ddl);
+                if (empty(trim($ddl))) {
+                    continue;
+                }
+                $pdo->exec($ddl . ";");
+            }
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+
+        try {
+            $this->info("adding already ran migrations...");
+            $migrations = file_get_contents(
+                "{$current_dir}/../../../database/migrations/{$schema_name}/initial_migrations.sql",
+                true,
+            );
+
+            $migrations = explode(";", $migrations);
+
+            foreach ($migrations as $idx => $statement) {
+                if (empty(trim($statement))) {
+                    continue;
+                }
+                $pdo->exec($statement . ";");
+                $this->info("adding migration {$idx} ...");
+            }
+
+            $this->info(sprintf("Test DB for schema %s created successfully!", $schema_name));
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
     }
-  }
 
-  protected function validateOptions($schema): void {
-    $validator = Validator::make(
-      [
-        "schema" => $schema,
-      ],
-      [
-        "schema" => "required|string|in:" . implode(",", self::AllowedSchemas),
-      ],
-    );
+    protected function validateOptions($schema): void {
+        $validator = Validator::make(
+            [
+                "schema" => $schema,
+            ],
+            [
+                "schema" => "required|string|in:" . implode(",", self::AllowedSchemas),
+            ],
+        );
 
-    try {
-      $validator->validate();
-    } catch (ValidationException $e) {
-      $this->error("Validation error: " . $e->getMessage());
-      exit(1);
+        try {
+            $validator->validate();
+        } catch (ValidationException $e) {
+            $this->error("Validation error: " . $e->getMessage());
+            exit(1);
+        }
     }
-  }
 }

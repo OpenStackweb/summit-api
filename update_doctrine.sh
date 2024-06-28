d8b4a3e5138a2c6e0873fd939d05574ca3c75682
smarcet@gmail.com <smarcet@gmail.com>
2024-06-27 22:08:47 -0300

    chore: update doctrine cmd fix
    Change-Id: Ib7a493c6f7d2b71513a81f18c25b6e05e5b95d63

--- update_doctrine.sh
+++ update_doctrine.sh
@@ -5,8 +5,12 @@ php artisan route:cache
 php artisan view:clear
 php artisan view:cache
 
-php artisan doctrine:clear:metadata:cache
-php artisan doctrine:clear:query:cache
-php artisan doctrine:clear:result:cache
-php artisan doctrine:generate:proxies
+php artisan doctrine:clear:metadata:cache --no-interaction --em=model
+php artisan doctrine:clear:metadata:cache --no-interaction --em=config
+php artisan doctrine:clear:query:cache --em=model
+php artisan doctrine:clear:query:cache --em=config
+php artisan doctrine:clear:result:cache --no-interaction --em=model
+php artisan doctrine:clear:result:cache --no-interaction --em=config
+php artisan doctrine:generate:proxies --no-interaction --em=model
+php artisan doctrine:generate:proxies --no-interaction --em=config
 php artisan config:clear

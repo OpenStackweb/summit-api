7323396d2ab03b53ad1e2676e67d14d043b5c890
smarcet@gmail.com <smarcet@gmail.com>
2024-09-11 17:25:06 -0300

    chore: add measure middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \App\Http\Middleware\RequestMeasure::class,
         //\App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,

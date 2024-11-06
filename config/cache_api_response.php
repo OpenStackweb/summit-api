e861c3229ba1a2494b2a716e8d8a3fa088e6a7d0
smarcet@gmail.com <smarcet@gmail.com>
2024-11-06 14:47:53 -0300

    fix: fork api cache response time for get summit v2

--- config/cache_api_response.php
+++ config/cache_api_response.php
@@ -14,6 +14,7 @@
 
 return [
     'get_summit_response_lifetime'          => env('CACHE_API_RESPONSE_GET_SUMMIT_LIFETIME', 3600),
+    'get_summit_response_lifetime_v2'       => env('CACHE_API_RESPONSE_GET_SUMMIT_LIFETIME_V2', 300),
     'get_event_feedback_response_lifetime'  => env('CACHE_API_RESPONSE_GET_EVENT_FEEDBACK_LIFETIME', 600),
     'get_published_event_response_lifetime' => env('CACHE_API_RESPONSE_GET_PUBLISHED_EVENT_LIFETIME', 600),
     'get_summits_response_lifetime'         => env('CACHE_API_RESPONSE_GET_SUMMITS_LIFETIME', 600),

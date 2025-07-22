085d7e894ad435ed2c22705ff11765be0e0001e7
romanetar <roman_ag@hotmail.com>
2025-07-22 21:12:45 +0200

    fix: add on repository interface error
    Signed-off-by: romanetar <roman_ag@hotmail.com>

--- app/Models/Foundation/Summit/Repositories/ISummitSponsorshipAddOnRepository.php
+++ app/Models/Foundation/Summit/Repositories/ISummitSponsorshipAddOnRepository.php
@@ -1,4 +1,4 @@
-<?php namespace App\Repositories\Summit;
+<?php namespace App\Models\Foundation\Summit\Repositories;
 /**
  * Copyright 2025 OpenStack Foundation
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,77 +12,17 @@
  * limitations under the License.
  **/
 
-use App\Models\Foundation\Summit\Repositories\ISummitSponsorshipAddOnRepository;
-use App\Repositories\SilverStripeDoctrineRepository;
-use Doctrine\ORM\QueryBuilder;
 use models\summit\Summit;
-use models\summit\SummitSponsorshipAddOn;
-use utils\DoctrineFilterMapping;
-use utils\Filter;
-use utils\Order;
-
+use models\utils\IBaseRepository;
 /**
- * Class DoctrineSummitSponsorshipAddOnRepository
- * @package App\Repositories\Summit
+ * Interface ISummitSponsorshipAddOnRepository
+ * @package App\Models\Foundation\Summit\Repositories
  */
-final class DoctrineSummitSponsorshipAddOnRepository extends SilverStripeDoctrineRepository
-implements ISummitSponsorshipAddOnRepository
+interface ISummitSponsorshipAddOnRepository extends IBaseRepository
 {
-    /**
-     * @param QueryBuilder $query
-     * @param Filter|null $filter
-     * @param Order|null $order
-     * @return QueryBuilder
-     */
-    protected function applyExtraJoins(QueryBuilder $query, ?Filter $filter = null, ?Order $order = null): QueryBuilder
-    {
-        $query->innerJoin("e.sponsorship", "sps");
-        $query->innerJoin("sps.sponsor", "sp");
-        $query->innerJoin("sp.summit", "s");
-        return $query;
-    }
-
-    /**
-     * @return array
-     */
-    protected function getFilterMappings(): array
-    {
-        return [
-            'id'   => new DoctrineFilterMapping("e.id :operator :value"),
-            'name' => 'e.name',
-            'type' => 'e.type',
-            'sponsorship_id' => 'sps.id',
-            'sponsor_id' => 'sp.id',
-            'summit_id'  => 's.id',
-        ];
-    }
-
-    /**
-     * @return array
-     */
-    protected function getOrderMappings(): array
-    {
-        return [
-            'id'   => 'e.id',
-            'name' => 'e.name',
-            'type' => 'e.type',
-        ];
-    }
-
-    /**
-     * @return string
-     */
-    protected function getBaseEntity(): string
-    {
-       return SummitSponsorshipAddOn::class;
-    }
-
     /**
      * @param Summit $summit
      * @return array
      */
-    public function getMetadata(Summit $summit): array
-    {
-        return SummitSponsorshipAddOn::getMetadata();
-    }
+    public function getMetadata(Summit $summit): array;
 }

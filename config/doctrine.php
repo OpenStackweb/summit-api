46d31ad492f9e576f668c0175642ac1d2c00e713
sebastian marcet <smarcet@gmail.com>
2025-08-28 18:16:47 -0300

    chore: create an only write connection for migrations (#346)

--- config/doctrine.php
+++ config/doctrine.php
@@ -151,6 +151,72 @@ return [
             'middlewares' => array_filter([
                 env('DOCTRINE_LOGGING', false) ? Doctrine\DBAL\Logging\Middleware::class : null,
             ]),
+        ],
+        'model_write' => [
+            'dev'        => env('APP_DEBUG'),
+            'meta'       => env('DOCTRINE_METADATA', 'attributes'),
+            'quote_strategy' => EscapingQuoteStrategy::class,
+            'connection' => 'model_write',
+            'namespaces' => [
+                'App'
+            ],
+            'paths'      => [
+                base_path('app/Models/Foundation')
+            ],
+            'repository' => Doctrine\ORM\EntityRepository::class,
+            'proxies'    => [
+                'namespace'     => 'Proxies',
+                'path'          => storage_path('proxies'),
+                'auto_generate' => env('DOCTRINE_PROXY_AUTOGENERATE', false)
+            ],
+            /*
+            |--------------------------------------------------------------------------
+            | Doctrine events
+            |--------------------------------------------------------------------------
+            |
+            | The listener array expects the key to be a Doctrine event
+            | e.g. Doctrine\ORM\Events::onFlush
+            |
+            */
+            'events'     => [
+                'listeners'   => [
+                    Doctrine\ORM\Events::onFlush => App\Audit\AuditEventListener::class
+                ],
+                'subscribers' => []
+            ],
+            'filters'    => [],
+            /*
+            |--------------------------------------------------------------------------
+            | Doctrine mapping types
+            |--------------------------------------------------------------------------
+            |
+            | Link a Database Type to a Local Doctrine Type
+            |
+            | Using 'enum' => 'string' is the same of:
+            | $doctrineManager->extendAll(function (\Doctrine\ORM\Configuration $configuration,
+            |         \Doctrine\DBAL\Connection $connection,
+            |         \Doctrine\Common\EventManager $eventManager) {
+            |     $connection->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+            | });
+            |
+            | References:
+            | http://doctrine-orm.readthedocs.org/en/latest/cookbook/custom-mapping-types.html
+            | http://doctrine-dbal.readthedocs.org/en/latest/reference/types.html#custom-mapping-types
+            | http://doctrine-orm.readthedocs.org/en/latest/cookbook/advanced-field-value-conversion-using-custom-mapping-types.html
+            | http://doctrine-orm.readthedocs.org/en/latest/reference/basic-mapping.html#reference-mapping-types
+            | http://symfony.com/doc/current/cookbook/doctrine/dbal.html#registering-custom-mapping-types-in-the-schematool
+            |--------------------------------------------------------------------------
+            */
+            'mapping_types'              => [
+                'enum' => 'string'
+            ],
+            /**
+             * References:
+             * https://www.doctrine-project.org/projects/doctrine-dbal/en/current/reference/architecture.html#middlewares
+             */
+            'middlewares' => array_filter([
+                env('DOCTRINE_LOGGING', false) ? Doctrine\DBAL\Logging\Middleware::class : null,
+            ]),
         ]
     ],
     /*

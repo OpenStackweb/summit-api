86a0b049bca34c86d727a5ad0ed6598b572abbfd
smarcet <smarcet@gmail.com>
2025-09-09 22:26:55 -0300

    chore: optimize Many2OneExpandSerializer::serialize

--- Libs/ModelSerializers/Many2OneExpandSerializer.php
+++ Libs/ModelSerializers/Many2OneExpandSerializer.php
@@ -12,6 +12,7 @@
  * limitations under the License.
  **/
 
+use Illuminate\Support\Facades\Log;
 use ModelSerializers\SerializerRegistry;
 /**
  * Class Many2OneExpandSerializer
@@ -45,18 +46,38 @@ class Many2OneExpandSerializer extends One2ManyExpandSerializer
         if(!$testRuleRes) return $values;
         $values = $this->unsetOriginalAttribute($values);
         if($should_verify_relation && !in_array($this->attribute, $relations)) return $values;
-        $res = [];
-        foreach ($entity->{$this->getter}() as $item){
+
+
+         $childExpand    = AbstractSerializer::filterExpandByPrefix($expand, $this->attribute);
+         $childFields    = AbstractSerializer::filterFieldsByPrefix($fields, $this->attribute);
+         $childRelations = AbstractSerializer::filterFieldsByPrefix($relations, $this->attribute);
+         $registry       = SerializerRegistry::getInstance();
+         $res = [];
+
+         $items = $entity->{$this->getter}();
+         if ($items instanceof \Doctrine\ORM\PersistentCollection) {
+             Log::debug(sprintf("Many2OneExpandSerializer::serializer items instanceof \Doctrine\ORM\PersistentCollection"));
+             if (!$items->isInitialized()) $items->initialize();
+             $items = $items->toArray();
+         } elseif ($items instanceof \Doctrine\Common\Collections\Collection) {
+             Log::debug(sprintf("Many2OneExpandSerializer::serializer items instanceof \Doctrine\Common\Collections\Collection"));
+             $items = $items->toArray();
+         } elseif ($items instanceof \Illuminate\Support\Collection) {
+             Log::debug(sprintf("Many2OneExpandSerializer::serializer items instanceof \Illuminate\Support\Collection"));
+             $items = $items->all();
+         } elseif ($items instanceof \Traversable) {
+             Log::debug(sprintf("Many2OneExpandSerializer::serializer items instanceof \Traversable"));
+             $items = iterator_to_array($items, false);
+         } elseif (!is_array($items)) {
+             Log::debug(sprintf("Many2OneExpandSerializer::serializer items !is_array"));
+             $items = (array) $items;
+         }
+
+        foreach ($items as $item){
             $shouldSkip = is_null($this->should_skip_rule) ? false : call_user_func($this->should_skip_rule, $item, $params);
-            if ($shouldSkip == true) continue;
-            $res[] = SerializerRegistry::getInstance()->getSerializer($item, $this->serializer_type)
-                ->serialize
-                (
-                    AbstractSerializer::filterExpandByPrefix($expand, $this->attribute),
-                    AbstractSerializer::filterFieldsByPrefix($fields, $this->attribute),
-                    AbstractSerializer::filterFieldsByPrefix($relations, $this->attribute),
-                    $params
-                );
+            if ($shouldSkip) continue;
+            $res[] = $registry->getSerializer($item, $this->serializer_type)
+                ->serialize($childExpand, $childFields, $childRelations, $params);
         }
         $values[$this->attribute] = $res;
         return $values;

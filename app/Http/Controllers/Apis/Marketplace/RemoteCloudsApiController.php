174b1fc21eb213b812bea07c8b61124de021cfb1
Matias Perrone <matias.perrone@exomindset.co>
2025-09-30 11:57:21 -0300

    chore: Add controller's response OpenAPI schema

--- app/Http/Controllers/Apis/Marketplace/RemoteCloudsApiController.php
+++ app/Http/Controllers/Apis/Marketplace/RemoteCloudsApiController.php
@@ -83,43 +83,7 @@ final class RemoteCloudsApiController extends AbstractCompanyServiceApiControlle
             new OA\Response(
                 response: 200,
                 description: 'Success - Returns paginated list of remotely managed private clouds',
-                content: new OA\JsonContent(
-                    properties: [
-                        'total' => new OA\Property(property: 'total', type: 'integer', example: 5),
-                        'per_page' => new OA\Property(property: 'per_page', type: 'integer', example: 5),
-                        'current_page' => new OA\Property(property: 'current_page', type: 'integer', example: 1),
-                        'last_page' => new OA\Property(property: 'last_page', type: 'integer', example: 1),
-                        'data' => new OA\Property(
-                            property: 'data',
-                            type: 'array',
-                            items: new OA\Items(
-                                properties: [
-                                    'id' => new OA\Property(property: 'id', type: 'integer', example: 1),
-                                    'class_name' => new OA\Property(property: 'class_name', type: 'string', example: 'RemoteCloudService'),
-                                    'name' => new OA\Property(property: 'name', type: 'string', example: 'Managed OpenStack Cloud Service'),
-                                    'overview' => new OA\Property(property: 'overview', type: 'string', example: 'Remotely managed private OpenStack cloud'),
-                                    'call_2_action_url' => new OA\Property(property: 'call_2_action_url', type: 'string', example: 'https://example.com/managed-cloud'),
-                                    'slug' => new OA\Property(property: 'slug', type: 'string', example: 'managed-openstack-service'),
-                                    'company_id' => new OA\Property(property: 'company_id', type: 'integer', example: 1),
-                                    'type_id' => new OA\Property(property: 'type_id', type: 'integer', example: 1),
-                                    'is_compatible_with_storage' => new OA\Property(property: 'is_compatible_with_storage', type: 'boolean', example: true),
-                                    'is_compatible_with_compute' => new OA\Property(property: 'is_compatible_with_compute', type: 'boolean', example: true),
-                                    'is_compatible_with_federated_identity' => new OA\Property(property: 'is_compatible_with_federated_identity', type: 'boolean', example: true),
-                                    'is_compatible_with_platform' => new OA\Property(property: 'is_compatible_with_platform', type: 'boolean', example: true),
-                                    'is_openstack_powered' => new OA\Property(property: 'is_openstack_powered', type: 'boolean', example: true),
-                                    'is_openstack_tested' => new OA\Property(property: 'is_openstack_tested', type: 'boolean', example: true),
-                                    'openstack_tested_info' => new OA\Property(property: 'openstack_tested_info', type: 'string', example: 'Tested with OpenStack Bobcat'),
-                                    'hardware_spec' => new OA\Property(property: 'hardware_spec', type: 'string', example: 'High-performance servers with SSD storage'),
-                                    'pricing_models' => new OA\Property(property: 'pricing_models', type: 'string', example: 'Monthly subscription, Pay-as-you-use'),
-                                    'published_sla' => new OA\Property(property: 'published_sla', type: 'string', example: '99.9% uptime guarantee'),
-                                    'is_vendor_managed_upgrades' => new OA\Property(property: 'is_vendor_managed_upgrades', type: 'boolean', example: true)
-                                ],
-                                type: 'object'
-                            )
-                        )
-                    ],
-                    type: 'object'
-                )
+                content: new OA\JsonContent(ref: '#/components/schemas/PaginatedRemoteCloudsResponse')
             ),
             new OA\Response(response: Response::HTTP_PRECONDITION_FAILED, description: "Validation Error"),
             new OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: "Server Error")
--- app/Swagger/MarketplaceSchemas.php
+++ app/Swagger/MarketplaceSchemas.php
@@ -138,3 +138,52 @@ class MarketplaceAppliancesResponseSchema
 class PaginatedAppliancesResponseSchema
 {
 }
+
+#[OA\Schema(
+    schema: 'RemoteCloudsResponse',
+    type: 'object',
+    properties: [
+        'id' => new OA\Property(property: 'id', type: 'integer', example: 1),
+        'class_name' => new OA\Property(property: 'class_name', type: 'string', example: 'RemoteCloudService'),
+        'name' => new OA\Property(property: 'name', type: 'string', example: 'Managed OpenStack Cloud Service'),
+        'overview' => new OA\Property(property: 'overview', type: 'string', example: 'Remotely managed private OpenStack cloud'),
+        'call_2_action_url' => new OA\Property(property: 'call_2_action_url', type: 'string', example: 'https://example.com/managed-cloud'),
+        'slug' => new OA\Property(property: 'slug', type: 'string', example: 'managed-openstack-service'),
+        'company_id' => new OA\Property(property: 'company_id', type: 'integer', example: 1),
+        'type_id' => new OA\Property(property: 'type_id', type: 'integer', example: 1),
+        'is_compatible_with_storage' => new OA\Property(property: 'is_compatible_with_storage', type: 'boolean', example: true),
+        'is_compatible_with_compute' => new OA\Property(property: 'is_compatible_with_compute', type: 'boolean', example: true),
+        'is_compatible_with_federated_identity' => new OA\Property(property: 'is_compatible_with_federated_identity', type: 'boolean', example: true),
+        'is_compatible_with_platform' => new OA\Property(property: 'is_compatible_with_platform', type: 'boolean', example: true),
+        'is_openstack_powered' => new OA\Property(property: 'is_openstack_powered', type: 'boolean', example: true),
+        'is_openstack_tested' => new OA\Property(property: 'is_openstack_tested', type: 'boolean', example: true),
+        'openstack_tested_info' => new OA\Property(property: 'openstack_tested_info', type: 'string', example: 'Tested with OpenStack Bobcat'),
+        'hardware_spec' => new OA\Property(property: 'hardware_spec', type: 'string', example: 'High-performance servers with SSD storage'),
+        'pricing_models' => new OA\Property(property: 'pricing_models', type: 'string', example: 'Monthly subscription, Pay-as-you-use'),
+        'published_sla' => new OA\Property(property: 'published_sla', type: 'string', example: '99.9% uptime guarantee'),
+        'is_vendor_managed_upgrades' => new OA\Property(property: 'is_vendor_managed_upgrades', type: 'boolean', example: true)
+    ]
+)]
+class RemoteCloudsResponseSchema
+{
+}
+
+#[OA\Schema(
+    schema: 'PaginatedRemoteCloudsResponse',
+    allOf: [
+        new OA\Schema(ref: '#/components/schemas/PaginateDataSchemaResponse'),
+        new OA\Schema(
+            type: 'object',
+            properties: [
+                new OA\Property(
+                    property: 'data',
+                    type: 'array',
+                    items: new OA\Items(ref: '#/components/schemas/RemoteCloudsResponse')
+                )
+            ]
+        )
+    ]
+)]
+class PaginatedRemoteCloudsResponseSchema
+{
+}

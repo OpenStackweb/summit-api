2680eccde8c035cb239f9705c2f4cf8ddef838c2
Matias Perrone <matias.perrone@exomindset.co>
2025-10-06 19:00:36 -0300

    chore: add optional fields

--- app/Swagger/MarketplaceSchemas.php
+++ app/Swagger/MarketplaceSchemas.php
@@ -96,8 +96,6 @@ class PaginatedPublicOrPrivateCloudsResponseSchema
         'overview' => new OA\Property(property: 'overview', type: 'string', example: 'Complete OpenStack solution'),
         'call_2_action_url' => new OA\Property(property: 'call_2_action_url', type: 'string', example: 'https://example.com/contact'),
         'slug' => new OA\Property(property: 'slug', type: 'string', example: 'openstack-appliance'),
-        'company_id' => new OA\Property(property: 'company_id', type: 'integer', example: 1),
-        'type_id' => new OA\Property(property: 'type_id', type: 'integer', example: 1),
         'is_compatible_with_storage' => new OA\Property(property: 'is_compatible_with_storage', type: 'boolean', example: true),
         'is_compatible_with_compute' => new OA\Property(property: 'is_compatible_with_compute', type: 'boolean', example: true),
         'is_compatible_with_federated_identity' => new OA\Property(property: 'is_compatible_with_federated_identity', type: 'boolean', example: false),
@@ -105,6 +103,18 @@ class PaginatedPublicOrPrivateCloudsResponseSchema
         'is_openstack_powered' => new OA\Property(property: 'is_openstack_powered', type: 'boolean', example: true),
         'is_openstack_tested' => new OA\Property(property: 'is_openstack_tested', type: 'boolean', example: true),
         'openstack_tested_info' => new OA\Property(property: 'openstack_tested_info', type: 'string', example: 'Tested with OpenStack Yoga')
+    ],
+    anyOf: [
+        new OA\Schema(anyOf: [
+            'company_id' => new OA\Property(property: 'company_id', type: 'integer', example: 41),
+            'company' => new OA\Property(property: 'company', type: 'Company'),
+
+        ]),
+        new OA\Schema(anyOf: [
+            'type_id' => new OA\Property(property: 'type_id', type: 'integer', example: 13),
+            'type' => new OA\Property(property: 'type', type: 'MarketPlaceType'),
+        ]),
+        new OA\Property(property: 'reviews', type: 'array', items: new OA\Items(type: 'MarketPlaceReview', title: 'MarketPlaceReview')),
     ]
 )]
 class MarketplaceAppliancesResponseSchema {}

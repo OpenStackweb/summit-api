71c1decf2f2be1404f77eda0eb39684c146bb9ba
Matias Perrone <matias.perrone@exomindset.co>
2025-10-09 15:20:20 -0300

    feat: Extend Swagger Coverage for controller `OAuth2SummitTaxTypeApiController`

--- app/Swagger/SummitRegistrationSchemas.php
+++ app/Swagger/SummitRegistrationSchemas.php
@@ -4,6 +4,81 @@ namespace App\Swagger\schemas;
 
 use OpenApi\Attributes as OA;
 
+
+#[OA\Schema(
+    schema: 'SummitTaxType',
+    type: 'object',
+    properties: [
+        new OA\Property(property: 'id', type: 'integer', example: 1),
+        new OA\Property(property: 'created', type: 'integer', example: 1630500518),
+        new OA\Property(property: 'last_edited', type: 'integer', example: 1630500518),
+        new OA\Property(property: 'name', type: 'string', example: 'VAT'),
+        new OA\Property(property: 'tax_id', type: 'string', example: 'VAT-001', nullable: true),
+        new OA\Property(property: 'rate', type: 'number', format: 'float', example: 21.0),
+        new OA\Property(property: 'summit_id', type: 'integer', example: 42),
+        new OA\Property(
+            property: 'ticket_types',
+            type: 'array',
+            items: new OA\Items(oneOf: [
+                new OA\Schema(type: 'integer'),
+                new OA\Schema(type: 'SummitTicketType')
+            ]),
+            example: [1, 2, 3],
+            description: 'Array of ticket type IDs or its Model (only present when relations=ticket_types), expanded when expand includes ticket_types.'
+        ),
+    ]
+)]
+class SummitTaxTypeSchema
+{
+}
+
+#[OA\Schema(
+    schema: 'PaginatedSummitTaxTypesResponse',
+    allOf: [
+        new OA\Schema(ref: '#/components/schemas/PaginateDataSchemaResponse'),
+        new OA\Schema(
+            type: 'object',
+            properties: [
+                new OA\Property(
+                    property: 'data',
+                    type: 'array',
+                    items: new OA\Items(ref: '#/components/schemas/SummitTaxType')
+                )
+            ]
+        )
+    ]
+)]
+class PaginatedSummitTaxTypesResponseSchema
+{
+}
+
+#[OA\Schema(
+    schema: 'SummitTaxTypeCreateRequest',
+    type: 'object',
+    required: ['name', 'rate'],
+    properties: [
+        new OA\Property(property: 'name', type: 'string', example: 'VAT'),
+        new OA\Property(property: 'tax_id', type: 'string', example: 'VAT-001'),
+        new OA\Property(property: 'rate', type: 'number', format: 'float', example: 21.0, description: 'Rate must be greater than 0'),
+    ]
+)]
+class SummitTaxTypeCreateRequestSchema
+{
+}
+
+#[OA\Schema(
+    schema: 'SummitTaxTypeUpdateRequest',
+    type: 'object',
+    properties: [
+        new OA\Property(property: 'name', type: 'string', example: 'VAT'),
+        new OA\Property(property: 'tax_id', type: 'string', example: 'VAT-001'),
+        new OA\Property(property: 'rate', type: 'number', format: 'float', example: 21.0, description: 'Rate must be greater than 0'),
+    ]
+)]
+class SummitTaxTypeUpdateRequestSchema
+{
+}
+
 // Badge Types
 
 #[OA\Schema(
@@ -22,7 +97,7 @@ use OpenApi\Attributes as OA;
         )
     ]
 )]
-class PaginatedSummitBadgeTypesResponse
+class PaginatedSummitBadgeTypesResponseSchema
 {
 }
 
@@ -38,7 +113,7 @@ class PaginatedSummitBadgeTypesResponse
         new OA\Property(property: "is_default", type: "boolean", example: false),
     ]
 )]
-class SummitBadgeTypeCreateRequest
+class SummitBadgeTypeCreateRequestSchema
 {
 }
 
@@ -53,11 +128,83 @@ class SummitBadgeTypeCreateRequest
         new OA\Property(property: "is_default", type: "boolean", nullable: true, example: false),
     ]
 )]
-class SummitBadgeTypeUpdateRequest
+class SummitBadgeTypeUpdateRequestSchema
 {
 }
 
-//
+#[OA\Schema(
+    schema: 'SummitTaxType',
+    type: 'object',
+    properties: [
+        new OA\Property(property: 'id', type: 'integer', example: 1),
+        new OA\Property(property: 'created', type: 'integer', example: 1630500518),
+        new OA\Property(property: 'last_edited', type: 'integer', example: 1630500518),
+        new OA\Property(property: 'name', type: 'string', example: 'VAT'),
+        new OA\Property(property: 'tax_id', type: 'string', example: 'VAT-001', nullable: true),
+        new OA\Property(property: 'rate', type: 'number', format: 'float', example: 21.0),
+        new OA\Property(property: 'summit_id', type: 'integer', example: 42),
+        new OA\Property(
+            property: 'ticket_types',
+            type: 'array',
+            items: new OA\Items(oneOf: [
+                new OA\Schema(type: 'integer'),
+                new OA\Schema(type: 'SummitTicketType')
+            ]),
+            example: [1, 2, 3],
+            description: 'Array of ticket type IDs or its Model (only present when relations=ticket_types), expanded when expand includes ticket_types.'
+        ),
+    ]
+)]
+class SummitTaxTypeSchema
+{
+}
+
+#[OA\Schema(
+    schema: 'PaginatedSummitTaxTypesResponse',
+    allOf: [
+        new OA\Schema(ref: '#/components/schemas/PaginateDataSchemaResponse'),
+        new OA\Schema(
+            type: 'object',
+            properties: [
+                new OA\Property(
+                    property: 'data',
+                    type: 'array',
+                    items: new OA\Items(ref: '#/components/schemas/SummitTaxType')
+                )
+            ]
+        )
+    ]
+)]
+class PaginatedSummitTaxTypesResponseSchema
+{
+}
+
+#[OA\Schema(
+    schema: 'SummitTaxTypeCreateRequest',
+    type: 'object',
+    required: ['name', 'rate'],
+    properties: [
+        new OA\Property(property: 'name', type: 'string', example: 'VAT'),
+        new OA\Property(property: 'tax_id', type: 'string', example: 'VAT-001'),
+        new OA\Property(property: 'rate', type: 'number', format: 'float', example: 21.0, description: 'Rate must be greater than 0'),
+    ]
+)]
+class SummitTaxTypeCreateRequestSchema
+{
+}
+
+#[OA\Schema(
+    schema: 'SummitTaxTypeUpdateRequest',
+    type: 'object',
+    properties: [
+        new OA\Property(property: 'name', type: 'string', example: 'VAT'),
+        new OA\Property(property: 'tax_id', type: 'string', example: 'VAT-001'),
+        new OA\Property(property: 'rate', type: 'number', format: 'float', example: 21.0, description: 'Rate must be greater than 0'),
+    ]
+)]
+class SummitTaxTypeUpdateRequestSchema
+{
+}
 
 // Summit Badge Feature Types
 
@@ -77,7 +224,9 @@ class SummitBadgeTypeUpdateRequest
         )
     ]
 )]
-class PaginatedSummitBadgeFeatureTypesResponseSchema {}
+class PaginatedSummitBadgeFeatureTypesResponseSchema
+{
+}
 
 #[OA\Schema(
     schema: 'SummitBadgeFeatureTypeCreateRequest',
@@ -89,7 +238,9 @@ class PaginatedSummitBadgeFeatureTypesResponseSchema {}
         new OA\Property(property: 'template_content', type: 'string', example: '<div>{{name}}</div>'),
     ]
 )]
-class SummitBadgeFeatureTypeCreateRequestSchema {}
+class SummitBadgeFeatureTypeCreateRequestSchema
+{
+}
 
 #[OA\Schema(
     schema: 'SummitBadgeFeatureTypeUpdateRequest',
@@ -100,4 +251,6 @@ class SummitBadgeFeatureTypeCreateRequestSchema {}
         new OA\Property(property: 'template_content', type: 'string', example: '<div class="vip">{{name}}</div>'),
     ]
 )]
-class SummitBadgeFeatureTypeUpdateRequestSchema {}
+class SummitBadgeFeatureTypeUpdateRequestSchema
+{
+}

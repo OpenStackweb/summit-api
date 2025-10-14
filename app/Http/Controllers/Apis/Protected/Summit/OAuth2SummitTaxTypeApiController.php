cfb7d149cb66a10e5a16893d00756d2cb91b9107
Matias Perrone <matias.perrone@exomindset.co>
2025-10-14 14:50:03 -0300

    fix: Change "namespace" word positioning

--- app/Http/Controllers/Apis/Protected/Summit/OAuth2SummitTaxTypeApiController.php
+++ app/Http/Controllers/Apis/Protected/Summit/OAuth2SummitTaxTypeApiController.php
@@ -1,4 +1,7 @@
-<?php namespace App\Http\Controllers;
+<?php
+
+namespace App\Http\Controllers;
+
 /**
  * Copyright 2019 OpenStack Foundation
  * Licensed under the Apache License, Version 2.0 (the "License");

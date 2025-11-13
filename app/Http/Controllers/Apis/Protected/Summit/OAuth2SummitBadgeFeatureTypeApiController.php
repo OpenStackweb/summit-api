ebd95c402df016c612b605c2fdf9abe90efc0c69
Matias Perrone <github@matiasperrone.com>
2025-11-13 19:51:38 +0000

    chore: Move the security schema for the controller to its own file

--- app/Http/Controllers/Apis/Protected/Summit/OAuth2SummitBadgeFeatureTypeApiController.php
+++ app/Http/Controllers/Apis/Protected/Summit/OAuth2SummitBadgeFeatureTypeApiController.php
@@ -33,25 +33,6 @@ use OpenApi\Attributes as OA;
 use Exception;
 
 
-#[OA\SecurityScheme(
-        type: 'oauth2',
-        securityScheme: 'badge_feature_types_oauth2',
-        flows: [
-            new OA\Flow(
-                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-                flow: 'authorizationCode',
-                scopes: [
-                    SummitScopes::ReadAllSummitData => 'Read All Summit Data',
-                    SummitScopes::ReadSummitData => 'Read Summit Data',
-                    SummitScopes::WriteSummitData => 'Write Summit Data',
-                ],
-            ),
-        ],
-    )
-]
-class BadgeFeatureTypesOauth2Schema{}
-
 /**
  * Class OAuth2SummitBadgeFeatureTypeApiController
  * @package App\Http\Controllers

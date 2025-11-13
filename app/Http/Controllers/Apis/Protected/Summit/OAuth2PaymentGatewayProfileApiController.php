752af36153fe2a936272077584bed9ece550d6d9
Matias Perrone <github@matiasperrone.com>
2025-11-13 19:46:36 +0000

    chore: Move the security schema for the controller to its own file

--- app/Http/Controllers/Apis/Protected/Summit/OAuth2PaymentGatewayProfileApiController.php
+++ app/Http/Controllers/Apis/Protected/Summit/OAuth2PaymentGatewayProfileApiController.php
@@ -26,25 +26,6 @@ use ModelSerializers\SerializerRegistry;
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 
-#[OA\SecurityScheme(
-        type: 'oauth2',
-        securityScheme: 'summit_payment_gateway_oauth2',
-        flows: [
-            new OA\Flow(
-                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-                flow: 'authorizationCode',
-                scopes: [
-                    SummitScopes::ReadAllSummitData => 'Read all summit data',
-                    SummitScopes::ReadPaymentProfiles => 'Read payment profiles',
-                    SummitScopes::WriteSummitData => 'Write summit data',
-                    SummitScopes::WritePaymentProfiles => 'Write payment profiles',
-                ],
-            ),
-        ],
-    )
-]
-class OAuth2CPaymentGatewayProfileAPIAuthSchema{}
 
 /**
  * Class OAuth2PaymentGatewayProfileApiController

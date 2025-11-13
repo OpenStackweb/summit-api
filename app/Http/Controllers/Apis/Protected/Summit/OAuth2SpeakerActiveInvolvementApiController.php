e0a9988b09e50806e436e66e372aa40648bc0129
Matias Perrone <github@matiasperrone.com>
2025-11-13 19:48:17 +0000

    chore: Move the security schema for the controller to its own file

--- app/Http/Controllers/Apis/Protected/Summit/OAuth2SpeakerActiveInvolvementApiController.php
+++ app/Http/Controllers/Apis/Protected/Summit/OAuth2SpeakerActiveInvolvementApiController.php
@@ -25,27 +25,6 @@ use utils\PagingResponse;
 use Illuminate\Support\Facades\Request;
 use OpenApi\Attributes as OA;
 
-
-#[
-    OA\SecurityScheme(
-        type: 'oauth2',
-        securityScheme: 'speaker_active_involvement_oauth2',
-        flows: [
-            new OA\Flow(
-                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-                flow: 'authorizationCode',
-                scopes: [
-                    SummitScopes::ReadSummitData => 'Read Summit Data',
-                    SummitScopes::ReadAllSummitData => 'Read All Summit Data',
-                ],
-            ),
-        ],
-    )
-]
-class SpeakerActiveInvolvementAuthSchema{}
-
-
 /**
  * Class OAuth2SpeakerActiveInvolvementApiController
  * @package App\Http\Controllers

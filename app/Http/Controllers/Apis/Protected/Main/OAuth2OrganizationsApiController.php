4e97fffb975e64765463d657760438424b1954e8
Matias Perrone <github@matiasperrone.com>
2025-11-13 19:41:49 +0000

    chore: Move the security schema for the controller to its own file

--- app/Http/Controllers/Apis/Protected/Main/OAuth2OrganizationsApiController.php
+++ app/Http/Controllers/Apis/Protected/Main/OAuth2OrganizationsApiController.php
@@ -26,25 +26,6 @@ use ModelSerializers\SerializerRegistry;
 use OpenApi\Attributes as OA;
 
 
-#[OA\SecurityScheme(
-        type: 'oauth2',
-        securityScheme: 'organizations_oauth2',
-        flows: [
-            new OA\Flow(
-                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-                flow: 'authorizationCode',
-                scopes: [
-                    OrganizationScopes::WriteOrganizationData => 'Write Organization Data',
-                    OrganizationScopes::ReadOrganizationData => 'Read Organization Data',
-                ],
-            ),
-        ],
-    )
-]
-class RSVPAuthSchema{}
-
-
 /**
  * Class OAuth2OrganizationsApiController
  * @package App\Http\Controllers

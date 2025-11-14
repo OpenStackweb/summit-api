e63415188516a6f2d9d7e1ec3deef465800f9e28
Matias Perrone <github@matiasperrone.com>
2025-11-14 20:27:06 +0000

    chore: add page and per_page missing params

--- app/Http/Controllers/Apis/Marketplace/DistributionsApiController.php
+++ app/Http/Controllers/Apis/Marketplace/DistributionsApiController.php
@@ -41,6 +41,20 @@ final class DistributionsApiController extends AbstractCompanyServiceApiControll
         tags: ['Marketplace', 'Marketplace Distributions'],
         parameters: [
             new OA\Parameter(
+                name: 'page',
+                in: 'query',
+                required: false,
+                description: 'Page number for pagination',
+                schema: new OA\Schema(type: 'integer', example: 1)
+            ),
+            new OA\Parameter(
+                name: 'per_page',
+                in: 'query',
+                required: false,
+                description: 'Items per page',
+                schema: new OA\Schema(type: 'integer', example: 10, maximum: 100)
+            ),
+            new OA\Parameter(
                 name: 'filter[]',
                 in: 'query',
                 required: false,

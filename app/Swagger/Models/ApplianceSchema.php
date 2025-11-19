86778e899a817a86aac08766727f3f885e59e9ab
Matias Perrone <github@matiasperrone.com>
2025-11-19 22:03:58 +0000

    chore: add ref schemas

--- app/Swagger/Models/ApplianceSchema.php
+++ app/Swagger/Models/ApplianceSchema.php
@@ -26,14 +26,14 @@ use OpenApi\Attributes as OA;
         new OA\Property(property: 'openstack_tested_info', type: 'string', example: 'Tested with OpenStack Yoga'),
         new OA\Property(property: 'company_id', type: 'integer', example: 41, description: 'ID of the company that provides this appliance, visible only when is not expanded'),
         new OA\Property(property: 'company', ref: '#/components/schemas/Company', description: 'Company that provides this appliance, visible only when expanded'),
-        new OA\Property(property: 'type_id', type: 'integer', example: 13, description: 'ID of the type of this appliance, visible only when not expanded'),
-        new OA\Property(property: 'type', ref: '#/components/schemas/MarketPlaceType', description: 'Type of this appliance, visible only when expanded'),
-        new OA\Property(property: 'types', type: 'array', items: new OA\Items(ref: '#/components/schemas/MarketPlaceType'), description: 'Regional support of this appliance, visible only when requested as relation'),
+        new OA\Property(property: 'type_id', type: 'integer', example: 13, description: 'ID of the MarketPlaceType of this appliance, visible only when not expanded'),
         new OA\Property(property: 'reviews', type: 'array', items: new OA\Items(ref: '#/components/schemas/MarketPlaceReview', title: 'MarketPlaceReview'), description: 'Reviews of this appliance, visible only when expanded'),
         new OA\Property(property: 'capabilities', type: 'array', items: new OA\Items(ref: '#/components/schemas/OpenStackImplementationApiCoverage'), description: 'Capabilities of this appliance, visible only when requested as relation'),
         new OA\Property(property: 'hypervisors', type: 'array', items: new OA\Items(ref: '#/components/schemas/HyperVisorType'), description: 'Hypervisors of this appliance, visible only when requested as relation'),
         new OA\Property(property: 'guests', type: 'array', items: new OA\Items(ref: '#/components/schemas/GuestOSType'), description: 'GuestOSType of this appliance, visible only when requested as relation'),
         new OA\Property(property: 'supported_regions', type: 'array', items: new OA\Items(ref: '#/components/schemas/RegionalSupport'), description: 'Regional support of this appliance, visible only when requested as relation'),
+        // @TODO: Investigate how to reference properly, as it is present in ApplianceSerializer, but does not to have a serializer asociated
+        // new OA\Property(property: 'type', ref: '#/components/schemas/MarketPlaceType', description: 'MarketPlaceType of this appliance, visible only when expanded'),
     ],
 )]
 class ApplianceSchema

b58e49a9a5e5b55abcfdd2faabe60ef60d4ccd4d
Matias Perrone <github@matiasperrone.com>
2025-11-24 14:49:56 +0000

    chore: add description for complex expand models. Fix issues on params

--- app/Swagger/SummitRegistrationSchemas.php
+++ app/Swagger/SummitRegistrationSchemas.php
@@ -4,34 +4,6 @@ namespace App\Swagger\schemas;
 
 use OpenApi\Attributes as OA;
 
-
-#[OA\Schema(
-    schema: 'SummitTaxType',
-    type: 'object',
-    properties: [
-        new OA\Property(property: 'id', type: 'integer', example: 1),
-        new OA\Property(property: 'created', type: 'integer', example: 1630500518),
-        new OA\Property(property: 'last_edited', type: 'integer', example: 1630500518),
-        new OA\Property(property: 'name', type: 'string', example: 'VAT'),
-        new OA\Property(property: 'tax_id', type: 'string', example: 'VAT-001', nullable: true),
-        new OA\Property(property: 'rate', type: 'number', format: 'float', example: 21.0),
-        new OA\Property(property: 'summit_id', type: 'integer', example: 42),
-        new OA\Property(
-            property: 'ticket_types',
-            type: 'array',
-            items: new OA\Items(oneOf: [
-                new OA\Schema(type: 'integer'),
-                new OA\Schema(type: 'SummitTicketType')
-            ]),
-            example: [1, 2, 3],
-            description: 'Array of ticket type IDs or its Model (only present when relations=ticket_types), expanded when expand includes ticket_types.'
-        ),
-    ]
-)]
-class SummitTaxTypeSchema
-{
-}
-
 #[OA\Schema(
     schema: 'PaginatedSummitTaxTypesResponse',
     allOf: [

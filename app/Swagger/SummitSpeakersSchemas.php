cb3a395d802b78d4369b348110b28ff52a0f676f
Matias Perrone <github@matiasperrone.com>
2025-11-26 16:19:31 +0000

    chore: Add the correct security and x attributes and fix path with the correct routes

--- app/Swagger/SummitSpeakersSchemas.php
+++ app/Swagger/SummitSpeakersSchemas.php
@@ -16,14 +16,15 @@ use OpenApi\Attributes as OA;
         new OA\Property(property: 'email', type: 'string', format: 'email', example: 'speaker@example.com'),
         new OA\Property(property: 'first_name', type: 'string', example: 'John'),
         new OA\Property(property: 'last_name', type: 'string', example: 'Doe'),
-        new OA\Property(property: 'summit_id', type: 'integer', example: 1),
+        new OA\Property(property: 'summit_id', type: 'integer', example: 1, description: 'Summit ID'),
         new OA\Property(property: 'is_sent', type: 'boolean', example: false),
         new OA\Property(property: 'sent_date', type: 'integer', description: 'Unix timestamp', example: 1640995200, nullable: true),
         new OA\Property(
             property: 'tags',
             type: 'array',
-            items: new OA\Items(type: 'integer'),
-            example: [1, 2, 3]
+            items: new OA\Items(type: ['integer', 'string']),
+            example: [1, 2, 3],
+            description: 'Array of Tag IDs or names (when expanded) associated with the invitation',
         )
     ]
 )]

8eeaec9fb65492f3dfd4bd58e877490e7bfd39f6
Matias Perrone <github@matiasperrone.com>
2025-11-27 21:55:01 +0000

    feat: Enhance OpenAPI documentation for Summit Ticket Types API with OAuth2 security and request schemas

---
+++ app/Swagger/Security/SummitTicketTypesAuthSchema.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Swagger\Security;
+
+use App\Security\MemberScopes;
+use App\Security\SummitScopes;
+use OpenApi\Attributes as OA;
+
+#[
+    OA\SecurityScheme(
+    type: 'oauth2',
+    securityScheme: 'summit_ticket_types_oauth2',
+    flows: [
+        new OA\Flow(
+            authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
+            tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
+            flow: 'authorizationCode',
+            scopes: [
+                SummitScopes::ReadSummitData => 'Read Summit Data',
+                SummitScopes::ReadAllSummitData => 'Read All Summit Data',
+                SummitScopes::WriteSummitData => 'Write Summit Data',
+                SummitScopes::WriteTicketTypeData => 'Write Ticket Type Data',
+                MemberScopes::ReadMyMemberData => 'Read My Member Data',
+            ],
+        ),
+    ],
+)
+]
+class SummitTicketTypesAuthSchema
+{
+}

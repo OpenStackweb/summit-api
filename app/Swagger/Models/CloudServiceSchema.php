00e674aa2f6b15ba2d07a866a20693df57789041
Matias Perrone <github@matiasperrone.com>
2025-11-27 19:02:20 +0000

    chore: add requested changes

--- app/Swagger/Models/CloudServiceSchema.php
+++ app/Swagger/Models/CloudServiceSchema.php
@@ -12,8 +12,8 @@ use OpenApi\Attributes as OA;
         new OA\Schema(
             type: 'object',
             properties: [
-                new OA\Property(property: 'data_centers', type: 'array', items: new OA\Items(ref: '#/components/schemas/DataCenterLocation')),
-                new OA\Property(property: 'data_center_regions', type: 'array', items: new OA\Items(ref: '#/components/schemas/DataCenterRegion')),
+                new OA\Property(property: 'data_centers', type: 'array', items: new OA\Items(ref: '#/components/schemas/DataCenterLocation'), description: 'List of DataCenterLocation objects associated with this CloudService, only if ?relations=data_centers is used in the query string'),
+                new OA\Property(property: 'data_center_regions', type: 'array', items: new OA\Items(ref: '#/components/schemas/DataCenterRegion'), description: 'List of DataCenterRegion objects associated with this CloudService, only if ?relations=data_center_regions is used in the query string'),
             ]
         ),
     ],

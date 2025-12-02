13f06c172db293c4d548b1ce7be12abb04217bdf
Matias Perrone <github@matiasperrone.com>
2025-12-02 21:46:01 +0000

    chore: add operationId and fix namespace in security schema

--- app/Swagger/Security/PresentationActionTypesAuthSchema.php
+++ app/Swagger/Security/PresentationActionTypesAuthSchema.php
@@ -1,26 +1,28 @@
 <?php
 
-namespace App\Swagger\Security;
+namespace App\Swagger\schemas;
 
 use App\Security\SummitScopes;
 use OpenApi\Attributes as OA;
 
 #[
     OA\SecurityScheme(
-        type: 'oauth2',
-        securityScheme: 'presentation_action_types_oauth2',
-        flows: [
-            new OA\Flow(
-                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-                flow: 'authorizationCode',
-                scopes: [
-                    SummitScopes::ReadAllSummitData => 'Read All Summit Data',
-                    SummitScopes::ReadSummitData => 'Read Summit Data',
-                    SummitScopes::WriteSummitData => 'Write Summit Data',
-                ],
-            ),
-        ],
-    )
+    type: 'oauth2',
+    securityScheme: 'presentation_action_types_oauth2',
+    flows: [
+        new OA\Flow(
+            authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
+            tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
+            flow: 'authorizationCode',
+            scopes: [
+                SummitScopes::ReadAllSummitData => 'Read All Summit Data',
+                SummitScopes::ReadSummitData => 'Read Summit Data',
+                SummitScopes::WriteSummitData => 'Write Summit Data',
+            ],
+        ),
+    ],
+)
 ]
-class PresentationActionTypesAuthSchema{}
+class PresentationActionTypesAuthSchema
+{
+}

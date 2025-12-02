68545d79ca427055efbe70da3ff971bb244fd3a9
Matias Perrone <github@matiasperrone.com>
2025-12-02 21:24:36 +0000

    chore: change namespace

--- app/Swagger/Security/OrganizationsAuthSchema.php
+++ app/Swagger/Security/OrganizationsAuthSchema.php
@@ -1,25 +1,27 @@
 <?php
 
-namespace App\Swagger\Schemas;
+namespace App\Swagger\schemas;
 
 use App\Security\OrganizationScopes;
 use OpenApi\Attributes as OA;
 
 
 #[OA\SecurityScheme(
-        type: 'oauth2',
-        securityScheme: 'organizations_oauth2',
-        flows: [
-            new OA\Flow(
-                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-                flow: 'authorizationCode',
-                scopes: [
-                    OrganizationScopes::WriteOrganizationData => 'Write Organization Data',
-                    OrganizationScopes::ReadOrganizationData => 'Read Organization Data',
-                ],
-            ),
-        ],
-    )
+    type: 'oauth2',
+    securityScheme: 'organizations_oauth2',
+    flows: [
+        new OA\Flow(
+            authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
+            tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
+            flow: 'authorizationCode',
+            scopes: [
+                OrganizationScopes::WriteOrganizationData => 'Write Organization Data',
+                OrganizationScopes::ReadOrganizationData => 'Read Organization Data',
+            ],
+        ),
+    ],
+)
 ]
-class OrganizationsAuthSchema{}
+class OrganizationsAuthSchema
+{
+}

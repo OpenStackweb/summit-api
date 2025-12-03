d2b2d857ebad299aadd30f9c8034a3ad7777b4eb
Matias Perrone <github@matiasperrone.com>
2025-12-03 23:02:26 +0000

    fix: schemas names and fields

--- app/Swagger/SummitRegistrationSchemas.php
+++ app/Swagger/SummitRegistrationSchemas.php
@@ -5,50 +5,6 @@ namespace App\Swagger\schemas;
 use OpenApi\Attributes as OA;
 
 #[OA\Schema(
-    schema: 'SummitAttendee',
-    type: 'object',
-    properties: [
-        new OA\Property(property: 'summit_hall_checked_in', type: 'boolean'),
-        new OA\Property(property: 'summit_hall_checked_in_date', type: 'integer', example: 1630500518),
-        new OA\Property(property: 'summit_virtual_checked_in_date', type: 'integer', example: 1630500518),
-        new OA\Property(property: 'shared_contact_info', type: 'boolean'),
-        new OA\Property(property: 'member_id', type: 'integer'),
-        new OA\Property(property: 'summit_id', type: 'integer'),
-        new OA\Property(property: 'speaker_id', type: 'integer'),
-        new OA\Property(property: 'first_name', type: 'string'),
-        new OA\Property(property: 'last_name', type: 'string'),
-        new OA\Property(property: 'email', type: 'string'),
-        new OA\Property(property: 'company_id', type: 'integer'),
-        new OA\Property(property: 'disclaimer_accepted_date', type: 'integer', example: 1630500518),
-        new OA\Property(property: 'disclaimer_accepted', type: 'boolean'),
-        new OA\Property(property: 'admin_notes', type: 'string'),
-        new OA\Property(property: 'manager_id', type: 'integer'),
-        new OA\Property(property: 'tickets', type: 'array', items: new OA\Items(type: ['integer', 'SummitAttendeeTicket'])),
-        new OA\Property(property: 'extra_questions', type: 'array', items: new OA\Items(type: ['integer', 'SummitOrderExtraQuestionAnswer'])),
-        new OA\Property(property: 'presentation_votes', type: 'array', items: new OA\Items(type: ['integer', 'PresentationAttendeeVote'])),
-        new OA\Property(property: 'votes_count', type: 'integer'),
-        new OA\Property(property: 'ticket_types', type: 'object', items: new OA\Items(new OA\Items(
-            type: 'object',
-            properties: [
-                new OA\Property(property: 'type_id', type: 'integer'),
-                new OA\Property(property: 'qty', type: 'integer'),
-                new OA\Property(property: 'type_name', type: 'string'),
-            ]
-        ))),
-        new OA\Property(property: 'allowed_access_levels', type: 'array', items: new OA\Items(type: 'integer')),
-        new OA\Property(property: 'allowed_features', type: 'array', items: new OA\Items(type: ['integer', 'SummitBadgeFeatureType'])),
-        new OA\Property(property: 'speaker', type: 'PresentationSpeaker'),
-        new OA\Property(property: 'member', type: 'Member'),
-        new OA\Property(property: 'tags', type: 'array', items: new OA\Items(type: ['Tag', 'integer'])),
-        new OA\Property(property: 'company', type: 'Company'),
-        new OA\Property(property: 'manager', type: 'SummitAttendee'),
-    ]
-)]
-class SummitAttendee
-{
-}
-
-#[OA\Schema(
     schema: 'PaginatedSummitAttendeesResponse',
     allOf: [
         new OA\Schema(ref: '#/components/schemas/PaginateDataSchemaResponse'),
--- app/Swagger/schemas.php
+++ app/Swagger/schemas.php
@@ -139,20 +139,6 @@ class RSVPInvitationSchema {}
 class RSVPInvitationCSVSchema {}
 
 #[OA\Schema(
-    schema: 'SummitAttendee',
-    type: 'object',
-    properties: [
-        new OA\Property(property: 'id', type: 'integer'),
-        new OA\Property(property: 'created', type: 'integer', example: 1630500518),
-        new OA\Property(property: 'last_edited', type: 'integer', example: 1630500518),
-        new OA\Property(property: 'first_name', type: 'string', example: 'John'),
-        new OA\Property(property: 'last_name', type: 'string', example: 'Doe'),
-        new OA\Property(property: 'status', type: 'string', example: 'Complete'),
-    ]
-)]
-class SummitAttendeeSchema {}
-
-#[OA\Schema(
     schema: 'SummitEvent',
     type: 'object',
     properties: [

900345ad91796350e17cbf71ba192eb7ebea809a
Matias Perrone <matias.perrone@exomindset.co>
2025-12-04 16:57:11 -0300

    Feature | Extend Swagger Coverage for controller `OAuth2UserStoriesApiController` (#389)
    * feat: Add OpenAPI documentation to "getAllUserStories" method
    - Add controller's response to OpenAPI schema
    
    * fix: Change "namespace" word positioning
    
    * feat: Add OpenAPI schemas for UserStory, Organization, Continent, File, and UserStoriesIndustry models
    
    * feat: Add changes requested
    
    ---------
    
    Co-authored-by: Matias Perrone <github@matiasperrone.com>

--- app/Swagger/schemas.php
+++ app/Swagger/schemas.php
@@ -417,3 +417,23 @@ class ChunkedFileUploadCompleteResponseSchema {}
 )]
 class ChunkedFileUploadRequestSchema {}
 
+// User Stories
+
+
+#[OA\Schema(
+    schema: 'PaginatedUserStoriesResponse',
+    allOf: [
+        new OA\Schema(ref: '#/components/schemas/PaginateDataSchemaResponse'),
+        new OA\Schema(
+            type: 'object',
+            properties: [
+                new OA\Property(
+                    property: 'data',
+                    type: 'array',
+                    items: new OA\Items(ref: '#/components/schemas/UserStory')
+                )
+            ]
+        )
+    ]
+)]
+class PaginatedUserStoriesResponseSchema {}

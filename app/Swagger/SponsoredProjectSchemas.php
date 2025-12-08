b18f0fc636882a2d71fb2d0ee9cae3f6e19a5bf1
Matias Perrone <github@matiasperrone.com>
2025-12-08 13:47:55 +0000

    fix: documentation build issues

--- app/Swagger/SponsoredProjectSchemas.php
+++ app/Swagger/SponsoredProjectSchemas.php
@@ -2,26 +2,7 @@
 
 namespace App\Swagger\schemas;
 
-use App\Security\SponsoredProjectScope;
 use OpenApi\Attributes as OA;
-
-#[OA\Schema(
-    schema: 'SponsoredProject',
-    title: 'SponsoredProject',
-    description: 'Represents a Sponsored Project',
-    type: 'object',
-    required: ['name'],
-    properties: [
-        new OA\Property(property: 'id', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'name', type: 'string'),
-        new OA\Property(property: 'slug', type: 'string'),
-        new OA\Property(property: 'description', type: 'string'),
-        new OA\Property(property: 'is_active', type: 'boolean'),
-        new OA\Property(property: 'created', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'last_edited', type: 'integer', format: 'int64'),
-    ]
-)]
-class SponsoredProjectSchemas {}
 
 #[OA\Schema(
     schema: 'PaginatedSponsoredProjectsResponse',
@@ -41,27 +22,7 @@ class SponsoredProjectSchemas {}
         )
     ]
 )]
-class PaginatedSponsoredProjectsResponse {}
-
-#[OA\Schema(
-    schema: 'ProjectSponsorshipType',
-    title: 'Project Sponsorship Type',
-    description: 'Represents a Project Sponsorship Type',
-    type: 'object',
-    required: ['name'],
-    properties: [
-        new OA\Property(property: 'id', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'name', type: 'string'),
-        new OA\Property(property: 'slug', type: 'string'),
-        new OA\Property(property: 'description', type: 'string'),
-        new OA\Property(property: 'is_active', type: 'boolean'),
-        new OA\Property(property: 'order', type: 'integer'),
-        new OA\Property(property: 'sponsored_project_id', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'created', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'last_edited', type: 'integer', format: 'int64'),
-    ]
-)]
-class ProjectSponsorshipType {}
+class PaginatedSponsoredProjectsResponseSchema {}
 
 #[OA\Schema(
     schema: 'PaginatedProjectSponsorshipTypesResponse',
@@ -81,24 +42,7 @@ class ProjectSponsorshipType {}
         )
     ]
 )]
-class PaginatedProjectSponsorshipTypesResponse {}
-
-#[OA\Schema(
-    schema: 'SupportingCompany',
-    title: 'Supporting Company',
-    description: 'Represents a Supporting Company for a Sponsorship Type',
-    type: 'object',
-    required: ['company_id'],
-    properties: [
-        new OA\Property(property: 'id', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'company_id', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'sponsorship_type_id', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'order', type: 'integer'),
-        new OA\Property(property: 'created', type: 'integer', format: 'int64'),
-        new OA\Property(property: 'last_edited', type: 'integer', format: 'int64'),
-    ]
-)]
-class SupportingCompany {}
+class PaginatedProjectSponsorshipTypesResponseSchema {}
 
 #[OA\Schema(
     schema: 'PaginatedSupportingCompaniesResponse',
@@ -118,23 +62,4 @@ class SupportingCompany {}
         )
     ]
 )]
-class PaginatedSupportingCompaniesResponse {}
-
-#[
-    OA\SecurityScheme(
-        type: 'oauth2',
-        securityScheme: 'sponsored_projects_oauth2',
-        flows: [
-            new OA\Flow(
-                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-                flow: 'authorizationCode',
-                scopes: [
-                    SponsoredProjectScope::Read => 'Read Sponsored Projects',
-                    SponsoredProjectScope::Write => 'Write Sponsored Projects',
-                ],
-            ),
-        ],
-    )
-]
-class SponsoredProjectsAuthSchema {}
+class PaginatedSupportingCompaniesResponseSchema {}

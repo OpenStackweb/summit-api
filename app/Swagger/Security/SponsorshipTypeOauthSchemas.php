eb81234a60eb667335d747ba4ca44a609e17f679
Matias Perrone <github@matiasperrone.com>
2025-12-09 20:42:56 +0000

    chore: Add PR's requested changes

--- app/Swagger/Security/SponsorshipTypeOauthSchemas.php
+++ app/Swagger/Security/SponsorshipTypeOauthSchemas.php
@@ -1,24 +1,6 @@
-<?php 
+<?php
 namespace App\Swagger\schemas;
 
 use App\Security\SummitScopes;
 use OpenApi\Attributes as OA;
 
-#[
-    OA\SecurityScheme(
-        type: 'oauth2',
-        securityScheme: 'summit_sponsorship_oauth2',
-        flows: [
-            new OA\Flow(
-                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-                flow: 'authorizationCode',
-                scopes: [
-                    SummitScopes::ReadSummitData => 'Read Sponsorship Types Data',
-                    SummitScopes::WriteSummitData => 'Write Sponsorship Types Data',
-                ],
-            ),
-        ],
-    )
-]
-class SponsorshipTypeAuthSchema {}

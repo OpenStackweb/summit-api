2590fa488caa9de75fe507bf02d3c64d6efaa7ae
Matias Perrone <github@matiasperrone.com>
2025-12-09 20:39:37 +0000

    fix: remove repeated schema SponsorshipType

--- app/Swagger/Models/SponsorshipTypeSchema.php
+++ app/Swagger/Models/SponsorshipTypeSchema.php
@@ -4,20 +4,49 @@ namespace App\Swagger\schemas;
 
 use OpenApi\Attributes as OA;
 
-
 #[OA\Schema(
     schema: 'SponsorshipType',
     type: 'object',
     properties: [
-        new OA\Property(property: 'id', type: 'integer', example: 1),
-        new OA\Property(property: 'created', type: 'integer', example: 1),
-        new OA\Property(property: 'last_edited', type: 'integer', example: 1),
-        new OA\Property(property: 'name', type: 'string', ),
-        new OA\Property(property: 'label', type: 'string', ),
-        new OA\Property(property: 'order', type: 'integer', ),
-        new OA\Property(property: 'size', type: 'string', ),
-    ])
-]
+        new OA\Property(
+            property: 'id',
+            type: 'integer',
+            description: 'Sponsorship type identifier'
+        ),
+        new OA\Property(
+            property: 'created',
+            type: 'integer',
+            format: 'int64',
+            description: 'Creation timestamp (UNIX epoch)'
+        ),
+        new OA\Property(
+            property: 'last_edited',
+            type: 'integer',
+            format: 'int64',
+            description: 'Last modification timestamp (UNIX epoch)'
+        ),
+        new OA\Property(
+            property: 'name',
+            type: 'string',
+            description: 'Sponsorship type name'
+        ),
+        new OA\Property(
+            property: 'label',
+            type: 'string',
+            description: 'Sponsorship type display label'
+        ),
+        new OA\Property(
+            property: 'order',
+            type: 'integer',
+            description: 'Display order'
+        ),
+        new OA\Property(
+            property: 'size',
+            type: 'string',
+            description: 'Sponsorship size category (Small, Medium, Large, Big)'
+        ),
+    ]
+)]
 class SponsorshipTypeSchema
 {
 }
--- app/Swagger/SponsorshipTypeSchemas.php
+++ app/Swagger/SponsorshipTypeSchemas.php
@@ -26,51 +26,6 @@ use Symfony\Component\HttpFoundation\Response;
 )]
 class PaginatedDataSponsorshipTypeSchemas {}
 
-#[OA\Schema(
-    schema: 'SponsorshipType',
-    type: 'object',
-    properties: [
-        new OA\Property(
-            property: 'id',
-            type: 'integer',
-            description: 'Sponsorship type identifier'
-        ),
-        new OA\Property(
-            property: 'created',
-            type: 'integer',
-            format: 'int64',
-            description: 'Creation timestamp (UNIX epoch)'
-        ),
-        new OA\Property(
-            property: 'last_edited',
-            type: 'integer',
-            format: 'int64',
-            description: 'Last modification timestamp (UNIX epoch)'
-        ),
-        new OA\Property(
-            property: 'name',
-            type: 'string',
-            description: 'Sponsorship type name'
-        ),
-        new OA\Property(
-            property: 'label',
-            type: 'string',
-            description: 'Sponsorship type display label'
-        ),
-        new OA\Property(
-            property: 'order',
-            type: 'integer',
-            description: 'Display order'
-        ),
-        new OA\Property(
-            property: 'size',
-            type: 'string',
-            description: 'Sponsorship size category (Small, Medium, Large, Big)'
-        ),
-    ]
-)]
-class SponsorshipTypeSchemas {}
-
 
 
 #[OA\Schema(

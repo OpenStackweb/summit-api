a14d09e1246317af9372589766ca6d47b8b4fe4f
Matias Perrone <github@matiasperrone.com>
2025-12-17 15:54:40 +0000

    chore: include PR requested changes

--- app/Http/Controllers/Apis/Protected/Summit/OAuth2SummitOrdersApiController.php
+++ app/Http/Controllers/Apis/Protected/Summit/OAuth2SummitOrdersApiController.php
@@ -46,30 +46,6 @@ use utils\Filter;
 use utils\FilterElement;
 use utils\PagingInfo;
 
-#[OA\SecurityScheme(
-    type: 'oauth2',
-    securityScheme: 'summit_orders_auth',
-    flows: [
-        new OA\Flow(
-            authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
-            tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
-            flow: 'authorizationCode',
-            scopes: [
-                SummitScopes::ReadAllSummitData => 'Read All Summit Data',
-                SummitScopes::ReadRegistrationOrders => 'Read Registration Orders',
-                SummitScopes::ReadMyRegistrationOrders => 'Read My Registration Orders',
-                SummitScopes::WriteSummitData => 'Write Summit Data',
-                SummitScopes::CreateRegistrationOrders => 'Create Registration Orders',
-                SummitScopes::CreateOfflineRegistrationOrders => 'Create Offline Registration Orders',
-                SummitScopes::DeleteRegistrationOrders => 'Delete Registration Orders',
-                SummitScopes::UpdateRegistrationOrders => 'Update Registration Orders',
-                SummitScopes::UpdateMyRegistrationOrders => 'Update My Registration Orders',
-                SummitScopes::DeleteMyRegistrationOrders => 'Delete My Registration Orders',
-            ],
-        ),
-    ],
-)
-]
 class OAuth2SummitOrdersApiControllerAuthSchema
 {
 }

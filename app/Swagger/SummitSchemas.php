3fadd505d9b0562d53aab0c79625de85d434e944
Matias Perrone <github@matiasperrone.com>
2025-12-29 20:00:38 +0000

    fix: merge conflicts and repeated operationId
    Signed-off-by: Matias Perrone <github@matiasperrone.com>

--- app/Swagger/SummitSchemas.php
+++ app/Swagger/SummitSchemas.php
@@ -343,30 +343,6 @@ class SummitAttendeeBadgeSchema
 )]
 class PaginatedSummitAttendeeBadgesResponseSchema {}
 
-#[OA\Schema(
-    schema: 'SummitSponsorshipType',
-    type: 'object',
-    properties: [
-        new OA\Property(property: 'id', type: 'integer', example: 1),
-        new OA\Property(property: 'widget_title', type: 'string', example: 'Platinum Sponsors', nullable: true),
-        new OA\Property(property: 'lobby_template', type: 'string', example: 'big-header', nullable: true),
-        new OA\Property(property: 'expo_hall_template', type: 'string', example: 'big-images', nullable: true),
-        new OA\Property(property: 'sponsor_page_template', type: 'string', example: 'big-header', nullable: true),
-        new OA\Property(property: 'event_page_template', type: 'string', example: 'big-header', nullable: true),
-        new OA\Property(property: 'sponsor_page_use_disqus_widget', type: 'boolean', example: true),
-        new OA\Property(property: 'sponsor_page_use_live_event_widget', type: 'boolean', example: true),
-        new OA\Property(property: 'sponsor_page_use_schedule_widget', type: 'boolean', example: true),
-        new OA\Property(property: 'sponsor_page_use_banner_widget', type: 'boolean', example: true),
-        new OA\Property(property: 'badge_image', type: 'string', example: 'https://example.com/badge.png', nullable: true),
-        new OA\Property(property: 'badge_image_alt_text', type: 'string', example: 'Platinum Badge', nullable: true),
-        new OA\Property(property: 'order', type: 'integer', example: 1),
-        new OA\Property(property: 'should_display_on_expo_hall_page', type: 'boolean', example: true),
-        new OA\Property(property: 'should_display_on_lobby_page', type: 'boolean', example: true),
-        new OA\Property(property: 'summit', type: 'Summit'),
-        new OA\Property(property: 'type', type: 'SponsorshipType'),
-    ]
-)]
-class SummitSponsorshipTypeSchema {}
 
 #[OA\Schema(
     schema: 'PaginatedSummitSponsorshipTypesResponse',

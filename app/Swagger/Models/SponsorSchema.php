504a1548ba62a4d8a40e12ca9c232752cc587381
Matias Perrone <github@matiasperrone.com>
2025-12-31 20:33:49 +0000

    chore: fix schemas and paths
    Signed-off-by: Matias Perrone <github@matiasperrone.com>

--- app/Swagger/Models/SponsorSchema.php
+++ app/Swagger/Models/SponsorSchema.php
@@ -7,17 +7,17 @@ use OpenApi\Attributes as OA;
 #[OA\Schema(
     schema: 'Sponsor',
     type: 'object',
-    properties: [
-        new OA\Property(property: 'id', type: 'integer', example: 1),
-        new OA\Property(property: 'company_id', type: 'integer', example: 1),
-        new OA\Property(property: 'sponsorship_type_id', type: 'integer', example: 1),
-        new OA\Property(property: 'order', type: 'integer', example: 1),
-        new OA\Property(property: 'badge_scans_count', type: 'integer', example: 0),
-        new OA\Property(property: 'links_count', type: 'integer', example: 0),
-        new OA\Property(property: 'company', type: 'object', description: 'Company object'),
-        new OA\Property(property: 'sponsorship', type: 'object', description: 'Sponsorship object'),
-        new OA\Property(property: 'is_published', type: 'boolean', example: true),
-        new OA\Property(property: 'sponsorships', type: 'array', items: new OA\Items(type: 'object'), nullable: true),
+    allOf: [
+        new OA\Schema(ref: '#/components/schemas/SponsorBase'),
+        new OA\Schema(
+            type: 'object',
+            properties: [
+                new OA\Property(property: 'sponsorship_id', type: 'integer'),
+                new OA\Property(property: 'sponsorship', ref: '#/components/schemas/SummitSponsorship', description: 'SummitSponsorship when expanded'),
+            ]
+        )
     ]
 )]
-class SponsorSchema {}
+class SponsorSchema
+{
+}

20fbad8c711e685f6c97c64c562dde94035ef58b
Matias Perrone <matias.perrone@exomindset.co>
2026-01-05 17:35:49 -0300

    Feature | Extend Swagger Coverage for controller `OAuth2TagsApiController` (#400)
    * feat: Extend Swagger Coverage for controller `OAuth2TagsApiController`
    
    * fix: param definition
    
    * fix: Change "namespace" word positioning
    
    * feat: Add OAuth2 security scheme
    
    * feat: Add changes requested in PR
    
    * feat: Add changes requested in PR
    
    * chore: Add missing properties in TagSchema
    
    Signed-off-by: Matias Perrone <github@matiasperrone.com>
    
    ---------
    
    Signed-off-by: Matias Perrone <github@matiasperrone.com>
    Co-authored-by: Matias Perrone <github@matiasperrone.com>

---
+++ app/Swagger/Security/TagsAuthSchema.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Swagger\schemas;
+
+use App\Security\SummitScopes;
+use OpenApi\Attributes as OA;
+
+#[
+    OA\SecurityScheme(
+        type: 'oauth2',
+        securityScheme: 'tags_oauth2',
+        flows: [
+            new OA\Flow(
+                authorizationUrl: L5_SWAGGER_CONST_AUTH_URL,
+                tokenUrl: L5_SWAGGER_CONST_TOKEN_URL,
+                flow: 'authorizationCode',
+                scopes: [
+                    SummitScopes::ReadAllSummitData => 'Read All Summit Data',
+                    SummitScopes::ReadSummitData => 'Read Summit Data',
+                    SummitScopes::ReadTagsData => 'Read Tags Data',
+                    SummitScopes::WriteSummitData => 'Write Summit Data',
+                    SummitScopes::WriteTagsData => 'Write Tags Data',
+                ],
+            ),
+        ],
+    )
+]
+class TagsAuthSchema {}

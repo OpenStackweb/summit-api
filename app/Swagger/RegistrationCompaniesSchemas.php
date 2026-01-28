3f6b87f2bd7e806d62aefb6a367501ddfec62645
Matias Perrone <github@matiasperrone.com>
2026-01-28 12:31:40 +0000

    fix: merge conflict
    Signed-off-by: Matias Perrone <github@matiasperrone.com>

--- app/Swagger/RegistrationCompaniesSchemas.php
+++ app/Swagger/RegistrationCompaniesSchemas.php
@@ -5,26 +5,6 @@ namespace App\Swagger\schemas;
 use OpenApi\Attributes as OA;
 
 #[OA\Schema(
-    schema: 'PaginatedCompaniesResponse',
-    type: 'object',
-    description: 'Paginated response for registration companies',
-    allOf: [
-        new OA\Schema(ref: '#/components/schemas/PaginateDataSchemaResponse'),
-        new OA\Schema(
-            type: 'object',
-            properties: [
-                new OA\Property(
-                    property: 'data',
-                    type: 'array',
-                    items: new OA\Items(ref: '#/components/schemas/Company')
-                )
-            ]
-        )
-    ]
-)]
-class PaginatedCompaniesResponseSchema {}
-
-#[OA\Schema(
     schema: 'ImportRegistrationCompaniesRequest',
     type: 'object',
     description: 'Request to import registration companies from CSV file',

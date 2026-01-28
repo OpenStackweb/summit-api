3502631154192a36ee63b54822b97b025d9d86ff
Matias Perrone <github@matiasperrone.com>
2026-01-28 15:35:33 +0000

    chore: fix rebase issues
    Signed-off-by: Matias Perrone <github@matiasperrone.com>

--- app/Swagger/SummitRegistrationSchemas.php
+++ app/Swagger/SummitRegistrationSchemas.php
@@ -958,26 +958,6 @@ class SummitBadgeTypeUpdateRequestSchema
 }
 
 #[OA\Schema(
-    schema: 'PaginatedSummitAttendeeTicketsResponse',
-    allOf: [
-        new OA\Schema(ref: '#/components/schemas/PaginateDataSchemaResponse'),
-        new OA\Schema(
-            type: 'object',
-            properties: [
-                new OA\Property(
-                    property: 'data',
-                    type: 'array',
-                    items: new OA\Items(ref: '#/components/schemas/SummitAttendeeTicket')
-                )
-            ]
-        )
-    ]
-)]
-class PaginatedSummitAttendeeTicketsResponseSchema
-{
-}
-
-#[OA\Schema(
     schema: 'RefundTicketRequest',
     type: 'object',
     required: ['amount'],

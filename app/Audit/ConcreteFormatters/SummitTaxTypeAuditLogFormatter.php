c0ba35db34aa87748e792ef39a2f42e8109222ee
Jose Andres Tejerina <andres.tejerina@exomindset.co>
2026-02-04 18:17:31 -0300

    feat: add new formatters for Tickets

---
+++ app/Audit/ConcreteFormatters/SummitTaxTypeAuditLogFormatter.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Audit\ConcreteFormatters;
+
+/**
+ * Copyright 2026 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+use App\Audit\AbstractAuditLogFormatter;
+use App\Audit\Interfaces\IAuditStrategy;
+use models\summit\SummitTaxType;
+use Illuminate\Support\Facades\Log;
+
+class SummitTaxTypeAuditLogFormatter extends AbstractAuditLogFormatter
+{
+    public function format($subject, array $change_set): ?string
+    {
+        if (!$subject instanceof SummitTaxType) {
+            return null;
+        }
+
+        try {
+            $name = $subject->getName() ?? 'Unknown';
+            $id = $subject->getId() ?? 'unknown';
+            $summit = $subject->getSummit();
+            $summit_name = $summit ? ($summit->getName() ?? 'Unknown Summit') : 'Unknown Summit';
+            $tax_id = $subject->getTaxId() ?? 'Not set';
+            $rate = ($subject->getRate() ?? 0) * 100;
+
+            switch ($this->event_type) {
+                case IAuditStrategy::EVENT_ENTITY_CREATION:
+                    $ticket_types_count = $subject->getTicketTypes()->count();
+                    return sprintf(
+                        "Tax Type '%s' (%d) created for Summit '%s': Tax ID %s, rate %.2f%%, applied to %d ticket types by user %s",
+                        $name,
+                        $id,
+                        $summit_name,
+                        $tax_id,
+                        $rate,
+                        $ticket_types_count,
+                        $this->getUserInfo()
+                    );
+
+                case IAuditStrategy::EVENT_ENTITY_UPDATE:
+                    $change_details = $this->buildChangeDetails($change_set);
+                    return sprintf(
+                        "Tax Type '%s' (%d) for Summit '%s' updated: %s by user %s",
+                        $name,
+                        $id,
+                        $summit_name,
+                        $change_details,
+                        $this->getUserInfo()
+                    );
+
+                case IAuditStrategy::EVENT_ENTITY_DELETION:
+                    $ticket_types_count = $subject->getTicketTypes()->count();
+                    return sprintf(
+                        "Tax Type '%s' (%d) for Summit '%s' (Tax ID: %s, rate: %.2f%%, applied to %d ticket types) was deleted by user %s",
+                        $name,
+                        $id,
+                        $summit_name,
+                        $tax_id,
+                        $rate,
+                        $ticket_types_count,
+                        $this->getUserInfo()
+                    );
+            }
+        } catch (\Exception $ex) {
+            Log::warning("SummitTaxTypeAuditLogFormatter error: " . $ex->getMessage());
+        }
+
+        return null;
+    }
+}

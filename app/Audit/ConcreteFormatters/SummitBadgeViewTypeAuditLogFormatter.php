bc5ed07874c4da2d73e313be32f266fe651bc4d6
Jose Andres Tejerina <andres.tejerina@exomindset.co>
2026-02-10 16:23:29 -0300

    feat: add new formatters for badges

---
+++ app/Audit/ConcreteFormatters/SummitBadgeViewTypeAuditLogFormatter.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Audit\ConcreteFormatters;
+
+/**
+ * Copyright 2026 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+use App\Audit\AbstractAuditLogFormatter;
+use App\Audit\Interfaces\IAuditStrategy;
+use models\summit\SummitBadgeViewType;
+use Illuminate\Support\Facades\Log;
+
+class SummitBadgeViewTypeAuditLogFormatter extends AbstractAuditLogFormatter
+{
+    public function format($subject, array $change_set): ?string
+    {
+        if (!$subject instanceof SummitBadgeViewType) {
+            return null;
+        }
+
+        try {
+            $name = $subject->getName() ?? 'Unknown Badge View Type';
+            $id = $subject->getId() ?? 'unknown';
+            $summit = $subject->getSummit();
+            $summit_name = $summit ? ($summit->getName() ?? 'Unknown Summit') : 'Unknown Summit';
+
+            switch ($this->event_type) {
+                case IAuditStrategy::EVENT_ENTITY_CREATION:
+                    return sprintf(
+                        "Badge View Type '%s' (%s) created in Summit '%s' by user %s",
+                        $name,
+                        $id,
+                        $summit_name,
+                        $this->getUserInfo()
+                    );
+
+                case IAuditStrategy::EVENT_ENTITY_UPDATE:
+                    $change_details = $this->buildChangeDetails($change_set);
+                    return sprintf(
+                        "Badge View Type '%s' (%s) in Summit '%s' updated: %s by user %s",
+                        $name,
+                        $id,
+                        $summit_name,
+                        $change_details,
+                        $this->getUserInfo()
+                    );
+
+                case IAuditStrategy::EVENT_ENTITY_DELETION:
+                    return sprintf(
+                        "Badge View Type '%s' (%s) from Summit '%s' was deleted by user %s",
+                        $name,
+                        $id,
+                        $summit_name,
+                        $this->getUserInfo()
+                    );
+            }
+        } catch (\Exception $ex) {
+            Log::warning("SummitBadgeViewTypeAuditLogFormatter error: " . $ex->getMessage());
+        }
+
+        return null;
+    }
+}

b030fb44e938abfe3f7db68f155b833c5622a84f
smarcet <smarcet@gmail.com>
2026-02-24 11:20:00 -0300

    chore(migration): add new migration for new PaymentGatewayProfile ApplicationType

---
+++ database/migrations/model/Version20260224141828.php
@@ -0,0 +1,42 @@
+<?php namespace Database\Migrations\Model;
+/**
+ * Copyright 2026 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260224141828 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add new Application type to payment profile';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $sql = <<<SQL
+ ALTER TABLE `PaymentGatewayProfile` CHANGE `ApplicationType` `ApplicationType` ENUM('Registration','BookableRooms','SponsorServices') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT 'Registration';
+SQL;
+
+        $this->addSql($sql);
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
